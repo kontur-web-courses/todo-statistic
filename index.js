aee39dda4cdd0c23a98bc6bdad22e0e8d75268c9
Upgrade220 <kowalkof@yandex.ru>
2020-09-28 15:20:53 +0500

    final Result

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 
+const allToDo = getAllTODO(files);
+
 console.log('Please, write your command!');
 readLine(processCommand);
 
@@ -12,14 +14,83 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    let commandSecondItem = command.substring(5,command.length);
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            consoleLogArrays(allToDo);
+            break;
+        case 'important' :
+            for(let todo of allToDo)
+                if(todo.indexOf('!') != -1)
+                    console.log(`important : ${todo}`);
+            break;
+        case `user ${commandSecondItem}` :
+            getUsersTODO(commandSecondItem);
+            break;
+        case `sort ${commandSecondItem}` :
+            getSortTODO(commandSecondItem);
         default:
             console.log('wrong command');
             break;
     }
 }
 
+function getSortTODO(command)
+{
+    switch(command)
+    {
+        case `importance`:
+            getSortImportanceTODO();
+        case `user`:
+            
+        case `date`:
+            getSortByDate();
+        default:
+            break;
+    }
+}
+
+function getSortByDate () {
+    let sortedList = allToDo.slice();
+    sortedList.sort((a,b) => a-b);
+    consoleLogArrays(sortedList);
+}
+
+function getSortImportanceTODO(){
+    let re = new RegExp('!');
+    let iCommentsMap = new Map();
+    for (let todo of allToDo){
+        iCommentsMap.set(todo, re.matchAll(todo).length);
+    }
+    let iComments = [...iCommentsMap].sort((a,b) => a-b);
+    consoleLogArrays(iComments);
+}
+
+function getUsersTODO(command){
+    for (let todo of allToDo){
+        let todoArr = todo.split(';');
+        if(todoArr[0].toLowerCase() === command.toLowerCase())
+            console.log(`user ${todoArr[0]} : ${todoArr[2]}`);
+    }
+}
+
+function getAllTODO (files){
+    let allTODO = [];
+    for (let file of files)
+    {
+        let lines = file.split('\n');
+        for(let i = 0; i < lines.length; i++)
+            if(lines[i].substring(0, 7) === '// TODO')
+                allTODO.push(lines[i].substring(8, lines[i].length));
+    }
+    return allTODO;
+}
+
+function consoleLogArrays(arr){
+    for(let el of arr)
+        console.log(el);
+}
 // TODO you can do it!
