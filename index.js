1055988ef1334a6a7ecd327e8ca05faced20f2c6
Alllex202 <Alllex.202@gmail.com>
2020-10-06 13:52:53 +0500

    refactor 1-5

--- index.js
+++ index.js
@@ -14,31 +14,55 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    let com = '';
     switch (command) {
         case 'exit':
-            console.log('COMMAND', command)
+            com = command;
+            // console.log('COMMAND', command)
             process.exit(0);
             break;
         case 'show':
             console.log('\r\nResults:\r\n');
-            console.log(COMMENTS, COMMENTS.length);
+            COMMENTS.map(comment => console.log(comment.text))
             break;
         case 'important':
-            COMMENTS.filter(c => c.important).map(c => console.log(c));
+            console.log('\r\nResults:\r\n');
+            COMMENTS
+                .filter(c => c.importance > 0)
+                .map(c => console.log(c.text));
             break;
-        case command.split(/user [a-zа-я0-9_\s]*/i)[1] === '' ? command : null:
-            const user = command.replace('user ', '').trim();
-            // console.log(user)
-            COMMENTS.filter(c => c.user === user ).map(c => console.log(c));
+        case command.match(/^user [a-zа-я0-9_\s]+$/gi) ? command : null:
+            const user = command.replace('user ', '').trim().toLowerCase();
+            console.log('\r\nResults:\r\n');
+            COMMENTS
+                .filter(c => c.user.toLowerCase() === user)
+                .map(c => console.log(c.text));
             break;
-        case command.split(/sort\s*importance/)[1] === '' ? command : null:
-            console.log(COMMENTS.sort((a, b) => a.important && b.important ? 0 : a.important ? -1 : b.important ? 1 : 0));
+        case command.match(/^sort importance$/gi) ? command : null:
+            console.log('\r\nResults:\r\n');
+            COMMENTS
+                .sort((a, b) => b.importance - a.importance)
+                .map(c => console.log(c.text));
+            // sortImportance();
             break;
-        case command.split(/sort\s*user/)[1] === '' ? command : null:
-            console.log(COMMENTS.sort((a, b) => (a.user && b.user) ? (a.user).localeCompare(b.user) : 0));
+        case command.match(/^sort user$/gi) ? command : null:
+            console.log('\r\nResults:\r\n');
+            COMMENTS
+                .sort((a, b) => (a.user && b.user) ? (a.user).localeCompare(b.user) : 0)
+                .map(c => console.log(c));
             break;
-        case command.split(/sort\s*date/)[1] === '' ? command : null:
-            console.log(COMMENTS.sort((a, b) => a.important && b.important ? 0 : a.important ? -1 : b.important ? 1 : 0));
+        case command.match(/^sort date$/gi) ? command : null:
+            console.log('\r\nResults:\r\n');
+            COMMENTS
+                .sort((a, b) => {
+                    return (a.date && b.date) ? b.date.date - a.date.date :
+                        a.date ? -1 : b.date ? 1 : 0;
+                })
+                .map(c => console.log(c));
+            break;
+        case command.match(/date [0-9]{4}(-[0-9]{2}(-[0-9]{2})?)?/i) ? command : null:
+            const date = command.match(/date [0-9]{4}(-[0-9]{2}(-[0-9]{2})?)?/);
+            console.log(date, date.length)
             break;
         default:
             console.log('wrong command');
@@ -47,29 +71,28 @@ function processCommand(command) {
 }
 
 // TODO you can do it!
+// TODO ; 2016; добавить writeLine!!!
+// TODO ; ; добавить writeLine!!!
 
 function getAllComments(files) {
     let comments = [];
     files.forEach(file => {
-        file.split('\r\n').forEach(line => {
-            const comment = line.split('// TODO ');
-            if (comment.length === 2 && (comment[0] + comment[1]).trim() !== 'const comment = line.split(\'\');') {
-                comments = [...comments, comment[1]];
-            }
-        });
+        const newComments = file.match(/\/\/ todo .+/gi);
+        comments.push(...newComments);
     });
+    console.log(comments)
     comments = comments.map(comment => {
-        const arr = comment.split(';').map(e => e.trim());
+        const arr = comment.split(/\/\/ todo /gi)[1].split(';').map(e => e.trim());
         let obj = {};
-        if (/!/.test(comment)) {
-            obj.important = true;
-        }
+        const importance = comment.match(/!/g) || [];
+        obj.importance = importance.length;
         if (arr.length === 1) {
             obj.text = arr[0].trim();
         } else if (arr.length === 3) {
             obj.user = arr[0].trim();
             const date = new Date(arr[1].trim());
             obj.date = {
+                date: date,
                 year: date.getFullYear(),
                 month: date.getMonth() + 1,
                 day: date.getDate(),
@@ -78,5 +101,19 @@ function getAllComments(files) {
         }
         return obj;
     });
+    console.log(comments)
     return comments;
 }
+
+// function sortImportance() {
+//     COMMENTS
+//         .map(comment => {
+//             const importance = comment.match(/!/g) || [];
+//             return {
+//                 text: comment,
+//                 importance: importance.length,
+//             };
+//         })
+//         .sort((a, b) => b.importance - a.importance)
+//         .map(comment => console.log(comment.text));
+// }
