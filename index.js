6357a3add5cbb1dfa02a4574c35a6323b270ca23
unknown <katya.shyb@yandex.ru>
2020-10-06 16:03:05 +0500

    Шубинцева

--- index.js
+++ index.js
@@ -12,14 +12,75 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    let [commands, arg] = command.split(" ")
+    switch (commands) {
         case 'exit':
             process.exit(0);
             break;
+            
+        case 'show':
+            for (const todo of getTodos())
+                console.log(todo);
+            break;
+        
+        case 'important':
+            for (const todo of getTodos().filter(value => value.includes("!")))
+                console.log(todo);
+            break;
+
+        case 'user':
+            let user = arg.toLowerCase();
+            for (const todo of getTodos().filter(value => value.toLowerCase().includes('\/\/ todo ' + user)))
+                console.log(todo);
+            break;
+
+        case 'sort':
+            console.log(sortBy(arg === 'important', arg === 'user', arg === 'date'))
+            break;
+
         default:
             console.log('wrong command');
             break;
     }
 }
 
+function getTodos() {
+    return files
+        .map(file => file.match(/\/\/.*todo.*/gi))
+        .flat(Infinity)
+        .filter(x => x)
+        .sort((a,b) => countItem(b, '!')-countItem(a, '!'))
+}
+
+//function todoSplit(todo){
+//    let arr = [];
+//    for (let i = 0; i<3; i++){
+//        arr[i] = todo.substr(8).split(';')[i];
+//        if (arr[i] != undefined)
+//            arr[i] = (arr[i][0] === ' ')? arr[i].substr(1) : arr[i];    
+//    }
+//    return(arr);
+//}
+
+function countItem(string, item){
+    return string.split('').reduce((p,i) => i === item? p + 1 :  p, 0)
+}
+
+function sortBy(important, user, date){
+    let todos = getTodos();
+    
+    if (important){
+        for (const todo of getTodos())
+                console.log(todo);
+    }
+
+    else if (user){
+        console.log(':(');
+    }
+
+    else if (date){
+        console.log(':(')
+    }
+
+}
 // TODO you can do it!
