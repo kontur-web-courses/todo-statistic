62d76fa0b6fec4298943416661fc4dcb28e3c071
Кирилл Пастухов <cyril@MacBook-Air-Kirill.local>
2020-10-07 16:15:43 +0500

    1-6, 11

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const {show} = require('./show');
-const {important} = require('./important');
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
+const path = require('path');
 
 const files = getFiles();
+const allTodos = findTodos(files);
 
 console.log('Please, write your command!');
 readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    //console.log(filePaths.map(path => readFile(path)))
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(filePath => {
+        return {
+            fileName: path.win32.basename(filePath),
+            data: readFile(filePath),
+        }
+    });
 }
 
 function processCommand(command) {
@@ -19,15 +23,51 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
-            
         case 'show':
-            show(getFiles());
+            console.log(allTodos);
             break;
-        
         case 'important':
-            important(getFiles());
+            console.log(
+                allTodos
+                .filter(todo => todo.importance > 0)
+            );
             break;
-
+        case command.match(/^user [a-zа-я0-9_\s]+$/g) ? command : null:
+            let user = command.replace('user ', '').trim().toLowerCase();
+            console.log(
+                allTodos
+                .filter(todo => todo.user && todo.user.toLowerCase() === user)
+            );
+            break;
+        case command.match(/^sort importance$/g) ? command : null:
+            console.log(
+                allTodos
+                .sort((a, b) => b.importance - a.importance)
+            );
+            break;
+        case command.match(/^sort user$/g) ? command : null:
+            console.log(
+                allTodos
+                .sort((a, b) => (a.user && b.user) ? (a.user).localeCompare(b.user, 'ru', {caseFirst: 'upper'}) :
+                    a.user ? -1 : b.user ? 1 : 0)
+            );
+            break;
+        case command.match(/^sort date$/g) ? command : null:
+            console.log(
+                allTodos
+                .sort((a, b) => {
+                    return (a.date && b.date) ? b.date.date - a.date.date :
+                        a.date ? -1 : b.date ? 1 : 0;
+                })
+            );
+            break;
+        case command.match(/^((date [0-9]{4}-[0-9]{2}-[0-9]{2}|date [0-9]{4}-[0-9]{2})|date [0-9]{4})$/g) ? command : null:
+            let date = new Date(command.match(/(([0-9]{4}-[0-9]{2}-[0-9]{2}|[0-9]{4}-[0-9]{2})|[0-9]{4})$/g)[0]);
+            console.log(
+                allTodos
+                .filter(todo => todo.date && todo.date.date > date)
+            );
+            break;
         default:
             console.log('wrong command');
             break;
@@ -35,4 +75,50 @@ function processCommand(command) {
 }
 
 // TODO you can do it!
+// TOdO:Переделать это
 
+function findTodos(files) {
+    let todos = [];
+
+    files.forEach(file => {
+        let newTodo = file.data.match(/\/\/\s?todo(\s|:).+/gi)
+        .map(element => {
+            return {
+                fileName: file.fileName,
+                comment: element,
+            }
+        });
+        todos.push(...newTodo);
+    });
+
+    todos = todos.map(todo => {
+        let arr = todo.comment.replace(/\/\/\s?todo(\s|:)/gi, '')
+        .split(';')
+        .map(element => element.trim());
+
+        let newTodo = {};
+        newTodo.fileName = todo.fileName;
+        let importance = todo.comment.match(/!/g) || [];
+        newTodo.importance = importance.length;
+
+        if (arr.length === 1) {
+            newTodo.message = arr[0].trim();
+        } else if (arr.length === 3) {
+            let user = arr[0].trim();
+            newTodo.user = user && user.length > 0 ? user : null;
+            let dateArray = arr[1].trim().split('-');
+            let date = new Date(arr[1].trim());
+            newTodo.date = {
+                date: date,
+                year: dateArray[0] ? Number(dateArray[0]) : null,
+                month: dateArray[1] ? Number(dateArray[1]) : null,
+                day: dateArray[2] ? Number(dateArray[2]) : null,
+            };
+            newTodo.message = arr[2].trim();
+        }
+        
+       return newTodo
+    });
+
+    return todos;
+}
