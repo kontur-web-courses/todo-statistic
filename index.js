c6754c620f9da247c1dc5c52409b0e43c3f75aef
Евгения <e.zhilinkova@yandex.ru>
2020-10-13 20:08:45 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
-
 const files = getFiles();
 
 console.log('Please, write your command!');
@@ -12,14 +11,54 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    let arr = getTodoArray();
+    let commandStr = command/*.toLowerCase()*/.split(/[\s;]+/);
+   
+    switch (commandStr[0]) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            arr.forEach(x => {
+                let index = x.indexOf("\/\/ TODO");
+                if (index != -1)
+                    console.log(x.slice(index));
+            });
+            break;
+        case 'important':
+            arr.forEach(x => {
+                let index = x.indexOf("\/\/ TODO");
+                if (index != -1) {
+                    let temp = x.slice(index);
+                    let important = temp.indexOf('\!');
+                    if(important != -1)
+                        console.log(temp);
+                }
+            });
+            break;
+        case 'user':     
+            arr.forEach(x => {
+                let index = x.indexOf("\/\/ TODO");
+                if (index != -1) {
+                    let temp = x.toLowerCase().slice(index);
+                    let important = temp.indexOf(commandStr[1]);
+                    if(important != -1)
+                        console.log(temp);
+                }     
+            });
+            break;
+        case '':   
+            break;   
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getTodoArray(callback) {
+    return getFiles()
+    .join('\n')
+    .split('\n');
 }
 
 // TODO you can do it!
