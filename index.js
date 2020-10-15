3dfac68ee99af8abd9f04775e0bde43fbeb274e3
iONicod <55252121+iONicod@users.noreply.github.com>
2020-10-16 00:20:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const {getComments} = require('./getComments');
+const {important} = require('./important');
+const {user} = require('./user');
+const {sort} = require('./sort');
+const {date} = require('./date');
+const {show} = require('./show');
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
@@ -12,10 +18,33 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    let name = command.replace(command.split(' ')[0] + ' ', '');
+    command = command.split(' ')[0];
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+            
+        case 'show':
+            show(getComments(files));
+            break;
+        
+        case 'important':
+            show(important(files));
+            break;
+
+        case 'user':
+            show(user(files, name));
+            break;
+
+        case 'sort':
+            show(sort(files, name));
+            break;
+
+        case 'date':
+            show(date(files, name))
+            break;
+
         default:
             console.log('wrong command');
             break;
