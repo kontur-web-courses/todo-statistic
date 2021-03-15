24be676409236fcdfcc79c355efb1f49ce627ed0
vadik <vadikverein45@gmail.com>
2021-03-15 12:37:37 +0500

    1-3

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const comments = getComments()
+console.log(comments);
+
+function getComments() {
+    let res = [];
+    for (let file of files) {
+        const regular_todo = /\/\/ TODO .+/g
+        let answ = file.match(regular_todo);
+        res = res.concat(answ);
+    }
+    return res;
+}
+
+
+function getImportant() {
+    const res = [];
+    let important = comments.filter(x => x.indexOf('!') !== -1);
+    res.push(important.join('\n'));
+    return res.join('\n');
+}
+
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -13,6 +34,12 @@ function getFiles() {
 
 function processCommand(command) {
     switch (command) {
+        case 'important':
+            console.log(getImportant());
+            break;
+        case 'show':
+            console.log(comments);
+            break;
         case 'exit':
             process.exit(0);
             break;
