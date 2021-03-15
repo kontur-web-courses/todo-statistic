3c718aaf52788b24cebd3888936dcf220d807ee1
yurcenko <yurchenko_s@tochka.com>
2021-03-15 13:23:41 +0500

    start sort

--- index.js
+++ index.js
@@ -20,8 +20,28 @@ function show(i){
 
 function userFinder(name){
     const data = getFiles().join("");
-    const regexpr = new RegExp('// TODO ' + name + ';(.*)\n','gi');
-    return Array.from(data.matchAll(regexpr)).map(x => x[1]).join("\n");
+    const regexpr = new RegExp('\/\/ TODO ' + name + ';(.*)\n','gi');
+    return Array.from(data.match(regexpr)).map(x => x[1]).join("\n");
+}
+
+
+function sortOutput(command){
+    const data = getFiles().join("");
+    const regexpr = /\/\/\sTODO\s(.*)\n/g;
+    const parsedData = data.match(regexpr);
+    switch (command) {
+        case 'importance':
+            return parsedData
+                .map(x => [x, x.matchAll(/(!+)\n/g)[1] || ''])
+                .map(x => [x[0], x[1].length])
+                .sort((a, b) => b[1] - a[1])
+                .map(a => a[0])
+                .join("");
+        case 'user':
+            break
+        case 'date':
+            break
+    }
 }
 
 function processCommand(command) {
