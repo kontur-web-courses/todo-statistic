24d96ec700e38328de1732495baefed70687ad97
YanaRyazanova <51403862+YanaRyazanova@users.noreply.github.com>
2021-03-15 04:12:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -12,9 +12,42 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    let command_split = command.split(' ')
+    switch (command_split[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(showCommand());
+            break;
+        case 'important':
+            let importants = getImportants();
+            let str = importants.join('\n');
+            console.log(str);
+            break;
+        case 'user':
+            let userName = command.split(' ')[1];
+            let res = showCommandsFromUser(userName).join('\n');
+            console.log(res);
+            break;
+        case 'sort':
+            switch(command_split[1]) {
+                case 'importance':
+                    console.log(sortImportants());
+                    break;
+                case 'user':
+                    let args = sortUser();
+                    let str1 = args.join('\n');
+                    console.log(str1);
+                    break;
+                case 'date':
+                    sortDate();
+                    break;
+            }
+            break;
+        case 'date':
+            let cur_date = command_split[1];
+            console.log(getDate(cur_date));
             break;
         default:
             console.log('wrong command');
@@ -22,4 +55,107 @@ function processCommand(command) {
     }
 }
 
+function sortDate(){
+    let all_args = showCommand();
+    for (let item of all_args){
+        let cur_date = get_date(item);
+        if (cur_date === -1)
+            continue;
+        sort_data(all_args);
+    }
+    console.log(all_args);
+}
+
+function get_date(item){
+    let dateReg = /\d{4}-\d{2}-\d{2}/;
+    let find_date = item.match(dateReg)
+    return find_date ? find_date[0]: -1;
+}
+
+function sort_data(array) {
+    array.sort(function(a,b){
+        return new Date(get_date(b)) - new Date(get_date(a));
+    });
+    return array;
+}
+
+function sortImportants(){
+    let importants_args = getImportants();
+    let all_args = showCommand();
+    let res = importants_args;
+    for (let item of all_args) {
+        if (importants_args.includes(item))
+            continue;
+        res.push(item);
+    }
+    return res;
+}
+
+function showCommand() {
+    let res = [];
+    let filesArr = files.toString().split('\n');
+    for (let file of filesArr) {
+        if (file.includes('// TODO')){
+            let i = file.indexOf('// TODO');
+            if (file.includes('file.includes(') || file.includes('file.indexOf'))
+                continue;
+            res.push(file.substr(i, file.length));
+        }
+    }
+    return res;
+}
+
+function getImportants() {
+    let res = [];
+    let todos = showCommand(files);
+    for (const todo of todos) {
+        if (todo.includes('!')) {
+            res.push(todo);
+        }
+    }
+    return res;
+}
+
+function sortUser() {
+    let named = [];
+    let unnamed = []
+    let todos = showCommand();
+    for (const todo of todos) {
+        let i = todo.indexOf(';');
+        if (i !== -1) {
+            named.push(todo.toLowerCase())
+        } else {
+            unnamed.push(todo.toLowerCase())
+        }
+    }
+    named.sort()
+    return named.concat(unnamed);
+}
+
+
+function showCommandsFromUser(user) {
+    let res = []
+    let todos = showCommand();
+    for (const todo of todos) {
+        let i = todo.indexOf(';');
+        let username = i === -1 ? '' : todo.slice(8, i);
+        if (user.toLowerCase() === username.toLowerCase())
+            res.push(todo);
+    }
+    return res;
+}
+
+function getDate(curDate){
+    let res = [];
+    let all_commands = showCommand();
+    let dates = curDate.split('-');
+    if (dates.length === 1){
+        for (let item of all_commands){
+            let thisDate = get_date(item);
+            if (new Date(get_date(thisDate)) - new Date(get_date(curDate)) >= 0)
+                res.push(this);
+        }
+    }
+    return res;
+}
 // TODO you can do it!
