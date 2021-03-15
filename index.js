a82fabb5f4d083264f0af68b4245bee6e11c7b21
yurcenko <yurchenko_s@tochka.com>
2021-03-15 12:43:14 +0500

    start sort

--- index.js
+++ index.js
@@ -11,10 +11,37 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function show(i){
+    const important = i || false
+    const data = getFiles().join("");
+    const regexpr = important ? /\/\/\sTODO\s(.*)!+\n/g : /\/\/\sTODO\s(.*)\n/g
+    return data.match(regexpr).join("");
+}
+
+function userFinder(name){
+    const data = getFiles().join("");
+    const regexpr = new RegExp('// TODO ' + name + ';(.*)\n','gi');
+    return Array.from(data.matchAll(regexpr)).map(x => x[1]).join("\n");
+}
+
 function processCommand(command) {
-    switch (command) {
+    const consoleAgrv = command.split(" ");
+    const com = consoleAgrv[0];
+    switch (com) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(show());
+            break;
+        case 'important':
+            console.log(show(true));
+            break;
+        case 'user':
+            console.log(userFinder(consoleAgrv[1]));
+            break;
+        case 'sort':
+            console.log(sortOutput(consoleAgrv[1]));
             break;
         default:
             console.log('wrong command');
