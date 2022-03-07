36bfb5ab9fe0999b199fae25b3ec22ddae215b1f
niripsa <anna.poluyanenko@gmail.com>
2022-03-07 12:05:20 +0500

    1

--- index.js
+++ index.js
@@ -11,10 +11,52 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+const todos = x => {let result = [];
+    for (let file of files)
+    {
+        let rows = file.split('\r\n').filter(x => x.indexOf('// todo '.toUpperCase()) !== -1 ).map(x => x.substr(x.indexOf('// todo '.toUpperCase()) + 8));
+        result = result.concat(rows);
+    }
+    return result;
+}
+
 function processCommand(command) {
-    switch (command) {
+    const a = command.split(' ')
+    switch (a[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log()
+            let d = todos()
+            if (d.length !== 0)
+                console.log(d.join('\n----------------------------------------------\n'));
+            break;
+        case 'important':
+            console.log()
+            let c = todos().filter(x => x.includes('!'))
+            if (c.length !== 0)
+                console.log(c.join('\n----------------------------------------------\n'));
+            break;
+        case 'user':
+            console.log()
+            let b = todos().filter(x => x.split(';')[0].toLowerCase() === a[1].toLowerCase())
+            if (b.length !== 0)
+                console.log(b.join('\n----------------------------------------------\n'))
+            break;
+        case 'sort':
+            console.log()
+            switch(a[1]) {
+                case 'importance':
+                    let k = todos().sort(x => x.split('').filter(y => y === '!').length)
+                    if (k.length !== 0)
+                        console.log(k.join('\n----------------------------------------------\n'))
+                    break;
+                case 'user':
+                    break;
+                case 'date':
+                    break;
+            }
             break;
         default:
             console.log('wrong command');
