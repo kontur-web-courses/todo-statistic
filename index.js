d96fa2acf70efd5144de5a973d2bc64ffa156376
EGREX <kirilmikov591@gmail.com>
2022-03-07 16:39:22 +0500

    add important

--- index.js
+++ index.js
@@ -3,17 +3,27 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 const todos = [];
+const important = [];
 
 console.log('Please, write your command!');
 
 for (file of files) {
     let strings = file.split('\n');
     for (str of strings) {
-        if (str.substring(0, 7) == '// TODO') todos.push(str);
+        if (str.substring(0, 7) == '// TODO') {
+            todos.push(str);
+                for (j of str.split("")){
+                if (j === '!'){
+                    important.push(str);
+                    break;
+                }   
+            }
+        }
     }
 }
 readLine(processCommand);
 readLine(processCommand('show'));
+readLine(processCommand('important'));
 readLine(processCommand('exit'));
 
 function getFiles() {
@@ -28,6 +38,9 @@ function processCommand(command) {
             break;
         case 'show':
             for (element of todos) console.log(element);
+            break;
+        case 'important':
+            for (element of important) console.log(element);
             break;
         default:
             console.log('wrong command');
