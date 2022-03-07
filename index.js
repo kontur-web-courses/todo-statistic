f7dbf453bffdf1c46845972ca928228b97d113e3
DILYARA <fatykhova.dilyara@mail.ru>
2022-03-07 19:42:20 +0500

    Taskdone

--- index.js
+++ index.js
@@ -11,11 +11,58 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+
 function processCommand(command) {
+    let username = '';
+    let result = [];
+    let importantResult = [];
+    let userResult = [];
+    if (command.includes('user')){
+        username = command.slice(5);
+        username.toLowerCase();
+    }
+    let importance = '';
+    let date = '';
+
+    for (let file of files){
+        const lines = file.split('\r\n');
+        for (let line of lines){
+            if (line.includes('// TODO') && !line.includes('))')){
+                result.push(line.slice(line.indexOf('// TODO')));
+            }
+        }
+    }
+
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+
+        case 'show':
+            console.log(result);
+            break;
+
+        case 'important':
+            for(let item of result){
+                if (item.includes('!')){
+                    importantResult.push(item);
+                }
+            }
+            console.log(importantResult);
+            break;
+
+        case `user ${username}`:
+            for(let item of result){
+                if (item.includes(`${username}`)){
+                    userResult.push(item);
+                }
+            }
+            console.log(userResult);
+            break;
+
+        case `sort ${importance} | ${username} | ${date}`:
+
+
         default:
             console.log('wrong command');
             break;
