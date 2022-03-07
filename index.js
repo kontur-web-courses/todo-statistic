0d145ea22f3c0364c58cf5ef46cbe1773bd63f6c
denisSurkov <surkov0denis@yandex.ru>
2022-03-07 12:50:03 +0500

    add getTodoComments

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const TODO_REGEX = /\/\/ TODO (.*)/gm
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,13 +12,28 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTodoComments() {
+    const allComments = [];
+    for (const file of files) {
+        const matched = [...file.matchAll(TODO_REGEX)];
+
+        allComments.push(...matched.map(match => match[0]));
+    }
+    return allComments;
+}
+
 const commands = {
     exit: () => process.exit(0),
-    show: () => console.log(),
+    show: () => console.log(getTodoComments()),
 }
 
 function processCommand(command) {
-    commands[command] | console.log('wrong command');
+    if (!commands.hasOwnProperty(command))  {
+        console.log('wrong command');
+        return
+    }
+
+    commands[command]();
 }
 
 // TODO you can do it!
