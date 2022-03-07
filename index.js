3720a2e678423edf28fa2f737e2899cfc6ce4cfa
Artemii Kurganov <aokurganov@mail.ru>
2022-03-07 13:15:09 +0500

    Task 1 done

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const allToDO = getAllTodo();
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -9,6 +10,16 @@ readLine(processCommand);
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
+}
+
+function getAllTodo() {
+    let result = []
+    for (let f of files){
+        const regex = '\/\/ TODO (.*)'
+        let found = [...f.matchAll(regex)].map(x => x[1])
+        result.push(...found)
+    }
+    return result;
 }
 
 function processCommand(command) {
