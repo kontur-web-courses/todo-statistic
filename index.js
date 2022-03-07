af986d797a25649b07be60249380be45c903f65b
KrauhinSemen <72562026+KrauhinSemen@users.noreply.github.com>
2022-03-07 04:28:02 -0700

    proba

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todos = [];
 
 console.log('Please, write your command!');
+
+for (file of files) {
+    let strings = file.split('\n');
+    for (str of strings) {
+        if (str.substring(0, 7) == '// TODO') todos.push(str);
+    }
+}
 readLine(processCommand);
+readLine(processCommand('show'));
+readLine(processCommand('exit'));
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
@@ -15,6 +25,9 @@ function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (element of todos) console.log(element);
             break;
         default:
             console.log('wrong command');
