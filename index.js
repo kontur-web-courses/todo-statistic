2f1c604f3dab9f196682b4fbe9eec3625fbdb2d7
yulya <yliuakomissarova@gmail.com>
2022-03-07 13:06:02 +0500

    index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const toDos = getToDO('\/\/ TODO [-?!;,. а-яА-ЯёЁ0-9a-zA-Z\s]+');
+const usernames = getToDO('\/\/ TODO [ а-яА-ЯёЁ0-9a-zA-Z\s]+');
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,10 +13,31 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getToDO(pattern) {
+    const regexp = new RegExp(pattern, 'g');
+    const matches = files.map(str => str.match(regexp).map(str => str.substring(8)));
+    return [].concat(...Array.from(matches));
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (let toDo of toDos){
+                console.log(toDo);
+            }
+            break;
+        case 'important':
+            for (let toDo of toDos){
+                if (toDo.toString().indexOf('!') > -1){
+                    console.log(toDo);
+                }
+            }
+            break;
+        case `user`:
+            console.log(usernames);
             break;
         default:
             console.log('wrong command');
