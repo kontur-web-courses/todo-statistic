b29edac62a0d0b955c785657a33cab19c6bdb5af
EGREX <kirilmikov591@gmail.com>
2022-03-07 17:53:10 +0500

    add sort importance and date

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const files = getFiles();
 const todos = [];
 const important = [];
+const importantSorted = []
+let importantIndex = {}
+const date = [];
+const withoutDate = [];
 
 console.log('Please, write your command!');
 
@@ -17,12 +21,46 @@ for (file of files) {
                     break;
                 }   
             }
+            var find_info = str.split(";");
+            if (find_info.length == 3) date.push({info: str, date : new Date(find_info[1].substring(1))});
+            else withoutDate.push(str);
         }
     }
 }
+
+for (i of important){
+    importantIndex[i] = 0
+    for (j of i.split('')){
+        if (j === '!'){
+            importantIndex[i] = importantIndex[i] + 1;
+        }
+    }
+}
+while (Object.keys(importantIndex).length > 0){
+    let max = 0;
+    let str = "";
+    for (i of Object.keys(importantIndex)){
+        if (importantIndex[i] > max){
+            max = importantIndex[i];
+            str = i;
+        }
+    }
+    importantSorted.push(str)
+    delete importantIndex[str]
+}
+
+for (i of todos){
+    if (importantSorted.indexOf(i) === -1){
+        importantSorted.push(i)
+    }
+}
+
+
 readLine(processCommand);
 readLine(processCommand('show'));
 readLine(processCommand('important'));
+readLine(processCommand('sort importance'));
+readLine(processCommand('sort date'));
 readLine(processCommand('exit'));
 
 function getFiles() {
@@ -41,6 +79,22 @@ function processCommand(command) {
         case 'important':
             for (element of important) console.log(element);
             break;
+        case 'sort importance':
+            for (element of importantSorted) console.log(element);
+            break;
+        case 'sort date':
+            for (var i = date.length - 1; i > 0; i--) {
+                for (var j = 0; j < i; j++) {
+                    if (date[j].date < date[j + 1].date) {
+                        let temp = date[j];
+                        date[j] = date[j + 1];
+                        date[j + 1] = temp;
+                    }
+                }
+            }
+            for (element of date) console.log(element.info);
+            for (element of withoutDate) console.log(element);
+            break;
         default:
             let sCommand = command.split(' ');
             if (sCommand[0] == 'user'){
@@ -56,3 +110,5 @@ function processCommand(command) {
 
 // TODO you can do it!
 // TODO Semyon; 11.11.11; {текст комментария}lcxmbvcb
+// TODO gsdhgfhdh!!!!!!!!!!!!!!!!!!!!!
+// TODO sdjfh!!!!!!!!!
