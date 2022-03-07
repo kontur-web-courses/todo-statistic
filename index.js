6bb1d678b11f5da73e6f9357b81a4f3e27199e67
MatveyMatveyIl <matvey.ilichev@gmail.com>
2022-03-07 11:21:29 +0500

    show

--- index.js
+++ index.js
@@ -16,10 +16,32 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            showTODO();
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
 
+function findTODO() {
+    const allTODO = [];
+    for(const file of files){
+        const lines = file.split('\n');
+        for(const line of lines){
+            const index = line.indexOf('// ' +
+                'TODO', 0);
+            if(index === -1) continue;
+            allTODO.push(line.slice(index + 8, line.length));
+        }
+    }
+    return allTODO;
+}
+
+function showTODO() {
+    for(const el of findTODO()){
+        console.log(el);
+    }
+}
 // TODO you can do it!
