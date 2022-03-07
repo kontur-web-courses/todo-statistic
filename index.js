1f27633e7ee9a41a00618c3c87707d5c12b8dc3e
BootyOne <72355779+BootyOne@users.noreply.github.com>
2022-03-07 19:51:02 +0500

    Ланец, Романенко, Самсонов. 5.5 пунктов реализовано

--- index.js
+++ index.js
@@ -11,11 +11,58 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function findAllTODO(re){
+    for (file of files){
+        let str = file.matchAll(re);
+        for (e of str){
+            console.log(e[0])
+        }
+    }
+}
+
+function findOtherTODO(re){
+    for (file of files){
+        let str = file.matchAll(re);
+        let res = []
+        for (e of str){
+            res.push(e[0])
+        }
+        res.flat();
+        res.sort(function (a, b) {
+            return b.split("!").length - 1 - a.split("!").length - 1
+        });
+        console.log(res)
+    }
+}
+
 function processCommand(command) {
+    let a = command.split(' ')
+    if (a.length > 1){
+        a = a.slice(1, a.length).join(' ')
+    }
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            findAllTODO(/\S*(\/\/ TODO .*)\r\n/g);
+            break;
+        case 'important':
+            findAllTODO(/\S*(\/\/ TODO .*!+)\r\n/g);
+            break;
+        case `user ${a}`:
+            findAllTODO(new RegExp('[\n.]*(\/\/ TODO ' + a + '.*)[\r\n]*', "ig"));
+            break;
+        case `user`:
+            findAllTODO(new RegExp('[\n.]*(\/\/ TODO .+;.*)[\r\n]*', "ig"));
+            findAllTODO(new RegExp('[\n.]*(\/\/ TODO (?!.+;).*)[\r\n]*', "ig"));
+            break;
+        case `sort ${a}`:
+            processCommand(a)
+            break;
+        case 'test':
+            findOtherTODO(/\S*(\/\/ TODO .*)\r\n/g);
+            break
         default:
             console.log('wrong command');
             break;
