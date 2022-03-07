b62fce698b2ee6463a596f9ff6c1fc3e7cd9e4d4
Kirill Novoselov <K.S.Novoselov@urfu.me>
2022-03-07 13:23:07 +0500

    1

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const {readLine} = require('./console');
 const files = getFiles();
 const TODO_REGEX = /\/\/ TODO (.*)/gm;
 const IMPORTANT_MARK = "!";
+const USER_DATE_COMMENT_REGEX = /(?:\s)*\/\/\sTODO\s(.+);\s(.+);\s(.+)/gm;
+
+const TODO_OBJECT = {
+    original: "",
+    comment: "",
+    date: "", // dateObject
+    user: "",
+    important_count: 0,
+}
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -13,11 +22,21 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function convertToTodoObject(todo){
+    let matched = todo.matchAll(USER_DATE_COMMENT_REGEX);
+    return {
+        original: todo,
+        user: matched[1],
+        date: matched[2],
+        comment: matched[3],
+        important_count: todo.split("!").length - 1,
+    }
+}
+
 function getTodoComments() {
     const allComments = [];
     for (const file of files) {
-        const matched = [...file.matchAll(TODO_REGEX)];
-
+        const matched = file.matchAll(TODO_REGEX).map(match => convertToTodoObject(match[0]));
         allComments.push(...matched.map(match => match[0]));
     }
     return allComments;
