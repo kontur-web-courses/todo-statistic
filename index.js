0a4ba0f473aa6b037234f9f7ba992516a7e01a5c
danilkaz <cmaster057@gmail.com>
2022-03-07 17:52:53 +0500

    add sort command (5)

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 
-const TODO_REGEX = new RegExp("\\/\\/ TODO ([\\w\\-]+);\\s*([\\w\\-]+);\\s*(.*)$");
+const TODO_REGEX = new RegExp("\\/\\/ TODO ([\\w\\- ]+);\\s*([\\w\\-]+);\\s*(.*)$");
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -15,22 +15,50 @@ function getFiles() {
 
 function processCommand(command) {
     let splitCommand = command.split(' ');
+    let todos = getAllTodos();
     switch (splitCommand[0]) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            console.log(getAllTodos());
+            console.log(todos);
             break;
         case 'important':
-            console.log(getAllTodos().filter(t => t.includes('!')))
+            console.log(todos.filter(t => t.includes('!')))
             break;
         case 'user':
             let user = splitCommand[1]
-            console.log(getAllTodos()
-                .map(t => t.match(TODO_REGEX))
-                .filter(t => t !== null && t[1].toLowerCase() === user.toLowerCase())
-                .map(t => t[0]));
+            console.log(findTodoByUser(todos, user));
+            break;
+        case 'sort':
+            switch (splitCommand[1]) {
+                case 'importance':
+                    console.log(todos.sort(
+                        (a, b) => countSymbols(b, '!') - countSymbols(a, '!')));
+                    break;
+                case 'user':
+                    let obj = {'noName': []};
+                    for (let todo of todos) {
+                        let matched = todo.match(TODO_REGEX);
+                        let user = 'noName';
+                        if (matched !== null)
+                            user = matched[1];
+                        if (!(user in obj))
+                            obj[user] = [];
+                        obj[user].push(todo);
+                    }
+                    for (let v in obj) {
+                        if (v !== 'noName')
+                            console.log(obj[v]);
+                    }
+                    console.log(obj['noName']);
+                    break;
+                case 'date':
+                    console.log(todos
+                        .map(t => t.match(TODO_REGEX) || [t, null, new Date(0)])
+                        .sort((a, b) => new Date(b[2]) - new Date(a[2]))
+                        .map(t => t[0]))
+            }
             break;
         default:
             console.log('wrong command');
@@ -50,6 +78,17 @@ function getAllTodos() {
         }
     }
     return result;
+}
+
+function findTodoByUser(todos, user) {
+    return getAllTodos()
+        .map(t => t.match(TODO_REGEX))
+        .filter(t => t !== null && t[1].toLowerCase() === user.toLowerCase())
+        .map(t => t[0]);
+}
+
+function countSymbols(string, symbol) {
+    return string.split(symbol).length - 1;
 }
 
 // TODO you can do it!
