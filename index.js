4c84e6aa524f23d3885ed2b684226c7166eb4976
innaanikina <avril-avril@mail.ru>
2022-03-07 18:11:10 +0500

    tasks 1,2 done

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
-const {readLine} = require('./console');
+const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
+const { readLine } = require('./console');
 
 const files = getFiles();
+const todos = [];
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,8 +12,33 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function makeTODO() {
+    for (let file of files) {
+        file = file.split("\n");
+        for (const line of file) {
+            let idx = line.indexOf("// TODO ");
+            if (idx > -1) {
+                let todo = line.substring(idx);
+                todos.push(todo);
+            }
+        }
+    }
+}
+
+function showTODO() {
+    if (todos.length === 0) {
+        makeTODO();
+    }
+    for (const todo of todos) {
+        console.log(todo);
+    }
+}
+
 function processCommand(command) {
     switch (command) {
+        case 'show':
+            showTODO();
+            break;
         case 'exit':
             process.exit(0);
             break;
@@ -22,4 +48,3 @@ function processCommand(command) {
     }
 }
 
-// TODO you can do it!
