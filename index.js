6afcc4d17b1f3ba159e7dfa838427231e071563d
Julia <juliachirkova@e1.ru>
2022-03-07 16:47:55 +0500

    done 1 and 2

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const {readLine} = require('./console');
 
 const files = getFiles();
 
+let arr = [];
+for (let i=0; i<files.length; i++) {
+    let file = files[i].split(/\r\n/);
+    for (let j = 0; j < file.length; j++) {
+        let index = file[j].indexOf('// TODO ');
+        if (index>-1) {
+            arr.push(file[j].substring(index));
+        }
+    }
+}
+
 console.log('Please, write your command!');
 readLine(processCommand);
 
@@ -15,6 +26,10 @@ function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (let s of arr)
+                console.log(s);
             break;
         default:
             console.log('wrong command');
