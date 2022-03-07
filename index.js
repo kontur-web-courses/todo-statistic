83893859a228cbed4382731cf0b6bac66132d248
Павел <xhbdlfkjgh@mail.ru>
2022-03-07 13:10:30 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
+const { type } = require('os');
 
 const files = getFiles();
 
@@ -12,14 +13,38 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    command = command.split(" ")
+    switch (command[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(readToDos(files))
+            break;
+        case 'important':
+            console.log(readToDos(files).filter(string => string.indexOf('!') !== -1));
+            break;
+        case 'user':
+            users = readToDos(files).map(string => string.slice(8, string.indexOf(';')))
+            // не доделал ;_)
             break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function readToDos(files){
+    todos = []
+    for (let file of files){
+        let strings = file.split('\n')
+        for (let line of strings){
+                let index = line.indexOf("\/\/ TODO ")
+                todostring = line.slice(index)
+                if (index !== -1) todos.push(todostring.substr(0,todostring.length - 1))
+        }
+    }
+    return todos
 }
 
 // TODO you can do it!
