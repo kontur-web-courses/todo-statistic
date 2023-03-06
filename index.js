2628c7ec534cf0465deb312f0b0182d402c7cebf
valitovaalina <92068055+valitovaalina@users.noreply.github.com>
2023-03-06 14:20:59 +0500

    Update index.js
    show

--- index.js
+++ index.js
@@ -16,10 +16,31 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            console.log();
+            let d = ToDoFind();
+            if (d.length !== 0) {
+                console.log(d.join('\n\n'));
+            }
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function ToDoFind() {
+    let list = [];
+    for (let f of files) {
+        for (let line of f.split('\n')) {
+            let index = line.indexOf('// TODO ')
+            if (index !== -1) {
+                list = list.concat(line.substring(index));
+            }
+        }
+    }
+
+    return list;
 }
 
 // TODO you can do it!
