99928c2f6eb4c5ba912e3f7941d9af32574d6f69
Ivan Sychev <funovichf@gmail.com>
2023-03-06 14:59:50 +0500

    4 completed

--- index.js
+++ index.js
@@ -12,15 +12,38 @@ function getFiles() {
 }
 
 function processCommand(command) {
+    let commandSplit = command.split(' ');
+    let name = ''
+    command = commandSplit[0];
+    if (commandSplit.length > 0) {
+        name = commandSplit[1]
+    }
     switch (command) {
+        case 'user':
+            for (const todo of parseToDo()) {
+                let todoSplit = todo.split(';', 3);
+
+                if (todoSplit.length !== 3)
+                    continue;
+
+                let user = todoSplit[0].split(' ')[2];
+                let date = todoSplit[1].trim();
+                let comment = todoSplit[2];
+
+                if (user.toLowerCase() === name.toLowerCase()) {
+                    console.log(todo);
+                }
+            }
+            break;
         case 'show':
             console.log(parseToDo());
-            break
+            break;
         case 'important':
             for (const todo of parseToDo()) {
                 if (todo.indexOf('!') !== -1)
                     console.log(todo);
             }
+            break;
         case 'exit':
             process.exit(0);
             break;
