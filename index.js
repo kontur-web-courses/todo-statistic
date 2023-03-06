9b9869aa949603a7388d287da858307777ccbd25
Matvey Lipilin <matvey.lipilin1@gmail.com>
2023-03-06 17:34:52 +0500

    some changes

--- index.js
+++ index.js
@@ -8,13 +8,32 @@ readLine(processCommand);
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+    console.log(filePaths);
     return filePaths.map(path => readFile(path));
+}
+
+function getAllTodos(fileData) {
+    const todos = [];
+    const todoRegExp = /\/\/\sTODO\s(.*)/g;
+    fileData.split('\n').forEach((line, index) => {
+        const match = todoRegExp.exec(line);
+        if (match) {
+            todos.push(match[1]);
+        }
+    });
+    return todos;
 }
 
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            files.forEach(file => {
+                const todos = getAllTodos(file);
+                todos.forEach(todo => console.log(todo));
+            });
             break;
         default:
             console.log('wrong command');
