9429a3b808f5ddfac1c4cad1b6052c83fea830f5
m.shkut <m.shkut@tinkoff.ru>
2023-03-06 14:20:27 +0500

    auth user wrapper

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const todos = getTodos(files);
+console.log(todos);
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -10,6 +12,19 @@ function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
+
+function getTodos(files) {
+    let res = [];
+    for (let text of files) {
+        for (let line of text.split('\n')){
+            if (line.startsWith("\/\/ TODO ")){
+                res.push(line.substring(8));
+            }
+        }
+    }
+    return res;
+}
+
 
 function processCommand(command) {
     switch (command) {
