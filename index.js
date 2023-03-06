1beafc4f4092b57ae484fe9cecc9de9e6db533f3
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2023-03-06 18:46:59 +0500

    feat: feat

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const { getAllFilePathsWithExtension, readFile } = require("./fileSystem");
 const { readLine } = require("./console");
-const fs = require("fs");
-const readline = require("readline");
-
-const files = getFiles();
+const todoString = "// " + "TODO";
 
 console.log("Please, write your command!");
 readLine(processCommand);
@@ -13,34 +10,21 @@ function getFiles() {
   return filePaths.map((path) => readFile(path));
 }
 
-function findTodoComments() {
-  const files = getFiles();
-  const todos = [];
-
-  for (const file of files) {
-    const contents = fs.readFileSync(file, "utf8");
-    const lines = contents.split("\n");
-
-    for (const line of lines) {
-      const match = line.match(/\/\/ TODO(?: (.*?);(?: (.*?);)?)? (.*)/i);
-      if (match) {
-        const [, author = "", date = "", text] = match;
-        todos.push({ file, line, author, date, text });
-      }
-    }
-  }
-
-  return todos;
-}
-
 function filterByUser(todos, username) {
   return todos.filter(
     (todo) => todo.user.toLowerCase() === username.toLowerCase()
   );
 }
 
+function filterByDates(todos) {
+    todos.sort(function(a,b){
+        return new Date(b.date) - new Date(a.date);
+      });
+    return todos;
+}
+
 function showTodos(todos) {
-  todos.forEach((todo) => console.log(`${todo.line} - ${todo.text}`));
+  todos.forEach((todo) => console.log('*' + todo.text));
 }
 
 function filterByImportance(todos) {
@@ -49,28 +33,43 @@ function filterByImportance(todos) {
   return [...important, ...other];
 }
 
+function filterByUsers(todos) {
+    let userNames = new Set();
+    let output = [];
+    todos.forEach(todo => {
+        userNames.add(todo.user);
+    });
+
+    userNames.forEach(userName => {
+        filterByUser(todos, userName).forEach(el => {
+            output.push(el);
+        })
+    });
+    return output;
+}
+
 function getTodos() {
   const todos = [];
   const files = getFiles();
   for (const file of files) {
     const lines = file.split("\n");
     lines.forEach((line, index) => {
-      if (line.includes("// TODO")) {
-        const startIndex = line.indexOf("// TODO") + 8;
+      if (line.includes(todoString)) {
+        const startIndex = line.indexOf(todoString) + 8;
         const endIndex =
-          line.indexOf("!", startIndex) === -1
+          line.lastIndexOf("!", startIndex) === -1
             ? line.length
-            : line.indexOf("!", startIndex);
+            : line.lastIndexOf("!");
         const todoText = line.substring(startIndex, endIndex + 1).trim();
         const userMatch = line.match(/\/\/ TODO (.+?);/i);
-        const dateMatch = line.match(/\/\/ TODO .+?; (.+)/i);
+        const dateMatch = new Date(todoText.split(';')[1]);
         todos.push({
           text: todoText,
           file,
           line: index + 1,
           priority: line.includes("!"),
           user: userMatch ? userMatch[1] : "unknown",
-          date: dateMatch ? dateMatch[1] : "unknown",
+          date: dateMatch
         });
       }
     });
@@ -94,11 +93,24 @@ function processCommand(command) {
     case "user":
       const username = command.substring(5).toLowerCase();
       showTodos(filterByUser(getTodos(), username));
+    case "sort":
+        switch (command.split(" ")[1]) {
+            case "importance":
+                showTodos(filterByImportance(getTodos()));
+                break;
+            case "user":
+                showTodos(filterByUsers(getTodos()));
+                break;
+            case "date":
+                showTodos(filterByDates(getTodos()));
+                break;
+            default:
+                console.log("Unknown filter");
+        }
+        break;
     case "exit":
       process.exit(0);
     default:
       console.log(`Unknown command: ${command}`);
   }
 }
-
-// TODO you can do it!
