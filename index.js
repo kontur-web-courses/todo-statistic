7ffbe4088488a763f8dd7a3f163a93473d2a6403
Sokolova Anna <anna.sok.03@mail.ru>
2023-03-06 17:31:33 +0500

    + show

--- index.js
+++ index.js
@@ -10,11 +10,29 @@ function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
+//использовать регулярные выражения
+function GetAllCommentsFromFiles(files) {
+    let comments = [];
+    for (let file of files) {
+        let comment = file.match(/\/\/ TODO .*/g);
+        if (comment != null) {
+            comments.push(comment);
+        }
+    }
+    return comments;
+}
+
 
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            let comments = GetAllCommentsFromFiles(getFiles());
+            for (let comment of comments) {
+                console.log(comment);
+            }
             break;
         default:
             console.log('wrong command');
