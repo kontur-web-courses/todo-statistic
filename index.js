9625c8bac0a1966d61d2dd5292854ad2a73e8b4e
Sokolova Anna <anna.sok.03@mail.ru>
2023-03-06 17:50:09 +0500

    + important

--- index.js
+++ index.js
@@ -16,22 +16,44 @@ function GetAllCommentsFromFiles(files) {
     for (let file of files) {
         let comment = file.match(/\/\/ TODO .*/g);
         if (comment != null) {
-            comments.push(comment);
+            for (const com of comment){
+                comments.push(com);
+            }
         }
     }
     return comments;
 }
 
 
+
+
 function processCommand(command) {
+    let comments = GetAllCommentsFromFiles(getFiles());
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            let comments = GetAllCommentsFromFiles(getFiles());
+            comments = GetAllCommentsFromFiles(getFiles());
             for (let comment of comments) {
                 console.log(comment);
+            }
+            break;
+        case 'important': //только с восклицательным знаком
+            comments = GetAllCommentsFromFiles(getFiles());
+            for (let comment of comments) {
+                if (comment.includes('!')) {
+                    console.log(comment);
+                }
+            }
+            break;
+        case ''://выводит комментарии, которые были добавлены конкретным пользователем
+            comments = GetAllCommentsFromFiles(getFiles());
+            for (let comment of comments) {
+                if (comment.includes(`${username};`)) {
+
+                    console.log(comment);
+                }
             }
             break;
         default:
