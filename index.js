44f30b167c84fbfc86c0236adb57428052193526
NikitaUnisikhin <nik.unisikhin.05@gmail.com>
2023-03-06 14:27:52 +0500

    aa

--- index.js
+++ index.js
@@ -11,10 +11,33 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getToDo(file) {
+    let toDoes = [];
+
+    let splitFile = [];
+    for (let row of file){
+        let split = row.split('\n');
+        for (let element of split) {
+            splitFile.push(element);
+        }
+    }
+
+    for (let row of splitFile){
+        if (row.includes("// TODO "))
+            toDoes.push(row);
+    }
+
+    return toDoes;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'todo':
+            for (let str of getToDo(getFiles()))
+                console.log(str);
             break;
         default:
             console.log('wrong command');
