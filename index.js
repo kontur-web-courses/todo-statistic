02a7d9cd4d5c987111c86ceca2386cfdc0e515c9
eyeless12 <maxim.chevychelov@gmail.com>
2023-03-06 14:44:11 +0500

    semi-full

--- index.js
+++ index.js
@@ -11,8 +11,85 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getAllTodos(){
+    let allTodos = []
+    for (let code of files){
+        for (let s of code.split('\n')) {
+            if (s.includes("// TODO")) {
+                let start = s.indexOf("/");
+                allTodos.push(s.substring(start));
+            }
+        }
+    }
+    return allTodos;
+}
+
+function getImportant(){
+    let allTodos = getAllTodos();
+    let importantTodos = []
+    for (let line of allTodos){
+        if (line.includes("!"))
+            importantTodos.push(line);
+    }
+    return importantTodos;
+}
+
+function getUserTodos(user){
+    let allTodos = getAllTodos();
+    let userTodos = []
+    for (let line of allTodos){
+        if (line.toLowerCase().includes(`// todo ${user};`))
+            userTodos.push(line);
+    }
+    return userTodos;
+}
+
+function getSort(arg){
+    switch (arg){
+        case 'importance':
+            return sortByImportance();
+        case 'user':
+            return sortByUser();
+        case 'date':
+            return sortByDate();
+    }
+}
+
+function sortByImportance() {
+    let allTodos = getAllTodos();
+    let todosByImportance = getImportant();
+    for (let line of allTodos) {
+        if (!todosByImportance.includes(line))
+            todosByImportance.push(line);
+    }
+    return todosByImportance;
+}
+
+function sortByUser(){
+    let allTodos = getAllTodos();
+}
+
+function sortByDate(){
+
+}
+
 function processCommand(command) {
-    switch (command) {
+    let splitedCommand = command.split(' ');
+    switch (splitedCommand[0]) {
+        case 'sort':
+            let arg = splitedCommand[1];
+            console.log(getSort(arg));
+            break;
+        case 'user':
+            let user = splitedCommand[1].toLowerCase();
+            console.log(getUserTodos(user))
+            break;
+        case 'important':
+            console.log(getImportant());
+            break;
+        case 'show':
+            console.log(getAllTodos());
+            break;
         case 'exit':
             process.exit(0);
             break;
@@ -21,5 +98,3 @@ function processCommand(command) {
             break;
     }
 }
-
-// TODO you can do it!
