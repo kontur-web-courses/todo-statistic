ad215494ffc3499c6d0779cf371ef031df7a29c5
Dyrda Lev <akronman0000@gmail.com>
2023-03-06 16:00:25 +0500

    Выполненное задание. Дырда Лев, Протасов Алексей ФТ-203

--- console.js
+++ console.js
@@ -9,7 +9,6 @@ function readLine(callback) {
         }
     });
 }
-
 // TODO digi; 2016-04-08; добавить writeLine!!!
 
 module.exports = {
--- index.js
+++ index.js
@@ -11,15 +11,78 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getToDoFromFile(file) {
+    return file
+        .split('\r\n')
+        .filter(x => x.includes('// TODO'))
+        .map(x => x.substring(x.indexOf('// TODO') + 8))
+        .map(x => x.split('; '))
+        .filter(x => x.length === 3)
+        .map(x => { return {user: x[0], date: x[1], comment: x[2], priority: x[2].includes('!')} });
+}
+
+function getAllToDo(){
+    let result = [];
+    for (let file of files)
+        result = result.concat(getToDoFromFile(file));
+
+    return result;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break
+        case 'show':
+            let todos = getAllToDo();
+            for (let todo of todos){
+                console.log(`${todo.user} ${todo.date} ${todo.comment}`)
+            }
+
+            break
+        case 'important':
+            let important = getAllToDo().filter(x => x.priority);
+            for (let e of important){
+                console.log(`${e.user} ${e.date} ${e.comment}`)
+            }
             break;
         default:
+            let args = command.split(' ');
+            if (args[0] === 'sort') {
+                let sortArg = args[1];
+                let sortPredicate;
+                let all = getAllToDo();
+
+                switch (sortArg) {
+                    case 'importance':
+                        sortPredicate = function (x, y) {return y.priority - x.priority};
+                        break;
+                    case 'user':
+                        sortPredicate = function (x, y) {return x.user.localeCompare(y.user)};
+                        break;
+                    case 'date':
+                        sortPredicate = function (x, y) {return new Date(x.date) - new Date(y.date)};
+                        break;
+                }
+
+                all = getAllToDo().sort(sortPredicate);
+                for (let e of all){
+                    console.log(`${e.user} ${e.date} ${e.comment}`)
+                }
+                break;
+            }
+            if (args[0] === 'date') {
+                let all = getAllToDo();
+                let date = args[1];
+                let after = all.filter(x => new Date(x.date) > new Date(date));
+                for (let e of after){
+                    console.log(`${e.user} ${e.date} ${e.comment}`)
+                }
+                break;
+            }
+
             console.log('wrong command');
             break;
     }
 }
-
-// TODO you can do it!
