a509c0234ae694162ea2e46dfd9e2b3dc8a7669f
Aratelnar (Boyakov M) <aratelnar@gmail.com>
2023-03-06 17:36:27 +0500

    push

--- index.js
+++ index.js
@@ -11,10 +11,22 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTODOs(text){
+    const reg = RegExp(/\/\/ TODO .*$/, "gm")
+    return text.match(reg)
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            getFiles().forEach((value, index, arr) => {
+                for (const v of getTODOs(value)) {
+                    console.log(v);
+                }
+            })
             break;
         default:
             console.log('wrong command');
