c0a500a916713577097c8a508315073f4bfb3088
Filipp <fil.agaponov@mail.ru>
2023-03-06 17:51:52 +0500

    Revert "visualization"
    This reverts commit d4b588ad862a38c28c37ff4fb03f835fc8bddf50.

--- index.js
+++ index.js
@@ -12,43 +12,14 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    const [cmd, arg] = command.split(" ");
-    switch (cmd) {
-        case "date":
-            printFormattedOutput(getNextDateTODO(getAllTODOs(), arg));
-            break;
-        case "sort":
-            printFormattedOutput(sortByParameter(getAllTODOs(), arg));
-            break;
-        case "user":
-            printFormattedOutput(getUserTODOs(getAllTODOs(), arg));
-            break;
-        case "important":
-            printFormattedOutput(getOnlyImportantTODOs(getAllTODOs()));
-            break;
-        case "show":
-            printFormattedOutput(getAllTODOs());
-            break;
-        case "exit":
+    switch (command) {
+        case 'exit':
             process.exit(0);
             break;
         default:
-            console.log("Unknown command");
+            console.log('wrong command');
             break;
     }
-}
-
-function getNextDateTODO(todos, date) {
-    return todos
-        .filter((el) => {
-            const parts = el.split(";");
-            return parts.length === 3 && new Date(parts[1]) > new Date(date);
-        })
-        .sort((a, b) => {
-            a = new Date(a.split(";")[1]);
-            b = new Date(b.split(";")[1]);
-            return a > b ? -1 : a < b ? 1 : 0;
-        });
 }
 
 function getAllTODOs() {
@@ -87,7 +58,7 @@ function sortByParameter(todos, parameter) {
                 .sort((a, b) => b.split("!").length - a.split("!").length);
         case "user":
             return todos
-                .sort((a, b) => a.localeCompare(b, undefined, {sensitivity: "base"}))
+                .sort((a, b) => a.localeCompare(b, undefined, { sensitivity: "base" }))
                 .sort((a, b) => b.split(";").length - a.split(";").length);
         case "date":
             const withDate = todos.filter((todo) => getTODOParts(todo).length === 3);
@@ -100,21 +71,11 @@ function sortByParameter(todos, parameter) {
     }
 }
 
-function printFormattedOutput(todos) {
-    todos.forEach((todo) => {
-        const [name, date, text] = getTodoParts(todo);
-        const importance = todo.includes("!") ? "!" : " ";
-        const formattedName = formatString(name, 11);
-        const formattedDate = formatString(date, 11);
-        const formattedText = formatString(text, 50);
-        console.log(`${importance}  |  ${formattedName}  |  ${formattedDate}  | ${formattedText}`);
-    });
-}
-
-function formatString(str, maxLength) {
-    if (str.length <= maxLength) {
-        return str;
-    } else {
-        return str.slice(0, maxLength - 3) + '...';
-    }
+function getNextDateTODO(todos, date) {
+    return todos
+        .filter((el) => {
+            const parts = el.split(";");
+            return parts.length === 3 && new Date(parts[1]) > new Date(date);
+        })
+        .sort((a, b) => new Date(b.split(";")[1]) - new Date(a.split(";")[1]));
 }
