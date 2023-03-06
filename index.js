60427bb1a1d89fb6fa7a623ce3ed4cc8f59f1b37
VladimirTambulatov <tambulatovv@yandex.ru>
2023-03-06 18:28:18 +0500

    Тамбулатов Хлызов ФТ202

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const files = getFiles();
 console.log('Please, write your command!');
 readLine(processCommand);
 
+function searchTodo(file){
+    let regexp = /\/\/ TODO [A-Za-z0-9А-Яа-я?!;: .,-]*/g
+    return  file.match(regexp)
+}
+
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
+    return filePaths.map(path => readFile(path))
 }
 
 function processCommand(command) {
@@ -16,6 +21,29 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            for (let file of getFiles(files)) {
+                console.log(searchTodo(file));
+            }
+            break
+        case 'important':
+            for (let file of getFiles(files)){
+                for (let str of searchTodo(file)){
+                    if (str.includes('!')) {
+                        console.log(str);
+                    }
+                }
+            }
+            break
+        case (command.match(/user [\wa-яё]+/i) || {}).input:
+            for (let file of getFiles(files)){
+                for (let str of searchTodo(file)){
+                    if (str.toLowerCase().includes(command.substring(5).toLowerCase())) {
+                        console.log(str);
+                    }
+                }
+            }
+            break
         default:
             console.log('wrong command');
             break;
