4adca051ee80a859f434f373ae24827c275c1dd5
bolthen <92172378+bolthen@users.noreply.github.com>
2023-03-06 15:12:47 +0500

    Add user command

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const strings = getFiles().map(f => f.split('\n')).reduce((a, b) => a.concat(b));
-const regex = /\/\/ TODO (.*)/;
+const todoRegex = /\/\/ TODO (.*)/;
+const namedTodoRegex = /\/\/ TODO (?<name>.+); *(?<date>.+); *(?<comment>.*)/;
+const userNameRegex = /user (?<name>.+)/;
+
+let namedTodoes = []
 
 function todo(strings){
     let result = []
     for(let str of strings){
-        let todo = str.match(regex);
+        let todo = str.match(todoRegex);
         if(todo)
             result.push(todo[0]);
+        let name = str.match(namedTodoRegex);
+        if (name) {
+            let obj = name.groups;
+            namedTodoes.push(obj);
+        }
     }
     return result;
 }
-let t = todo(strings);
+let allTodoes = todo(strings);
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -24,19 +33,28 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    commandSplited = command.split(' ');
+    switch (commandSplited[0]) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            for(let i of t){
+            for(let i of allTodoes){
                 console.log(i);
             }
             break;
         case 'important':
-            for (let i of t) {
+            for (let i of allTodoes) {
                 if (i.at(-1) === '!') {
                     console.log(i);
+                }
+            }
+            break;
+        case 'user':
+            let userName = command.match(userNameRegex).groups.name.toLowerCase();
+            for (let i of namedTodoes) {
+                if (i.name.toLowerCase() === userName) {
+                    console.log(i.comment);
                 }
             }
             break;
