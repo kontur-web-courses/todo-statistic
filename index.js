07f7e1d3daa7b31c582a24b0f0bd4e7d06c4fcb8
Akergez <ragusseven@gmail.com>
2023-03-06 14:35:46 +0500

    made parse todo

--- index.js
+++ index.js
@@ -11,10 +11,25 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function parseToDo(array) {
+    let result = []
+    for (let toDo of array) {
+        let curr = [toDo];
+        if(toDo.indexOf(';') !== -1)
+        {
+            curr.push(toDo.split(';')[0]);
+            curr.push(toDo.split(';')[1]);
+        }
+        result.push(curr);
+    }
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
             break;
         default:
             console.log('wrong command');
