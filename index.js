1d056a18ad8cae1373b67bb2bf4c6ffd695a82c9
D3RPole96 <ilya.lagunov2003@gmail.com>
2023-03-06 14:38:49 +0500

    important, user

--- index.js
+++ index.js
@@ -11,17 +11,45 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getAllTODOs() {
+    let TODOs = [];
+    for (let file of files){
+        for (let string of file.split('\n')) {
+            if (string.includes('// TODO ')
+                && !string.includes('\'// TODO ')
+                && !string.includes('\`// TODO ')) {
+                TODOs.push(string.substring(string.indexOf('// TODO')));
+            }
+        }
+    }
+
+    return TODOs;
+}
+
 function processCommand(command) {
-    switch (command) {
+    let cmd = command.split(' : ');
+    let operation = cmd[0].split(' ')[0];
+    let parameter = cmd[0].split(' ')[1];
+    switch (operation) {
         case 'exit':
             process.exit(0);
             break;
-        case 'show : показать все todo':
-            for (let file of files){
-                for (let string of file.split('\n')) {
-                    if (string.includes('// TODO ') && !string.includes('\'// TODO ')) {
-                        console.log(string.substring(string.indexOf('// TODO')));
-                    }
+        case 'show':
+            for (let TODO of getAllTODOs()) {
+                console.log(TODO);
+            }
+            break;
+        case 'important':
+            for (let TODO of getAllTODOs()) {
+                if (TODO.includes('!')) {
+                    console.log(TODO);
+                }
+            }
+            break;
+        case 'user':
+            for (let TODO of getAllTODOs()) {
+                if (TODO.includes(`// TODO ${parameter}`)) {
+                    console.log(TODO);
                 }
             }
             break;
