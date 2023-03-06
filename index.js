7f3c5753f529d616cb22d0b440e44d555ab0cff4
Denis <dlsayafarov@gmail.com>
2023-03-06 17:40:39 +0500

    bruhhhhhh

--- index.js
+++ index.js
@@ -2,25 +2,6 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
-
-console.log('Please, write your command!');
-readLine(processCommand);
-
-function getFiles() {
-    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
-    return filePaths.map(path => readFile(path));
-}
-
-function processCommand(command) {
-    switch (command) {
-        case 'exit':
-            process.exit(0);
-            break;
-        default:
-            console.log('wrong command');
-            break;
-    }
-}
 let todoArray = [];
 files.forEach((file, number) => {
     const lines = file.split('\n');
@@ -30,3 +11,31 @@ files.forEach((file, number) => {
         }
     });
 })
+
+console.log('Please, write your command!');
+readLine(processCommand);
+
+
+function processCommand(command) {
+    switch (command) {
+        case 'exit':
+            process.exit(0);
+            break;
+        case 'important':
+            console.log(getImportant())
+            break;
+        default:
+            console.log('wrong command');
+            break;
+    }
+}
+
+function getFiles() {
+    const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
+    return filePaths.map(path => readFile(path));
+}
+
+function getImportant()
+{
+    return todoArray.filter(s => s.includes('!'))
+}
