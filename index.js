377aa880933dd342420d8625b80dbe5c3a1ca937
Евгений <evgeniisergeev2002@list.ru>
2023-03-06 14:00:18 +0500

    q

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const files = getFiles();
 console.log('Please, write your command!');
 readLine(processCommand);
 
+
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
