2fc1f31630af4a312d707ca7bffa0671039cd65c
Denis <dlsayafarov@gmail.com>
2023-03-06 18:25:54 +0500

    bruhh x2

--- index.js
+++ index.js
@@ -6,26 +6,31 @@ let todoArray = [];
 files.forEach((file, number) => {
     const lines = file.split('\n');
     lines.forEach((line, index) => {
-        if (line.startsWith('// TODO')) {
-            todoArray.push(line);
+        let f = line.search(/(\/\/ TODO)[^'"]/g)
+        if (f != -1) {
+            todoArray.push(line.slice(f, ));
         }
     });
 })
 
+console.log(todoArray);
 console.log('Please, write your command!');
 readLine(processCommand);
 
 
 function processCommand(command) {
-    switch (command) {
-        case 'exit':
+    switch (true) {
+        case command === 'exit':
             process.exit(0);
             break;
-        case 'important':
+        case command === 'show':
+            todoArray.map(t => console.log(t))
+            break;
+        case command === 'important':
             console.log(getImportant())
             break;
-        case 'show : показать все todo':
-            todoArray.map(t => console.log(t))
+        case command.startsWith('sort'):
+            console.log(sortBy(command.split(' ')[1]))
             break;
         default:
             console.log('wrong command');
@@ -40,5 +45,22 @@ function getFiles() {
 
 function getImportant()
 {
-    return todoArray.filter(s => s.includes('!'))
+    return todoArray.filter(s => s.includes('!'));
+}
+
+function getNotImportant()
+{
+    return todoArray.filter(s => !s.includes('!'));
+}
+
+function sortBy(key)
+{
+    switch (key)
+    {
+        case 'importance':
+            return getImportant().concat(getNotImportant());
+        default:
+            console.log('wrong key');
+            break;
+    }
 }
