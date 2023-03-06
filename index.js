f87a70c176b774c5a66d10869c44681c281ad696
Akergez <ragusseven@gmail.com>
2023-03-06 15:18:29 +0500

    add table

--- index.js
+++ index.js
@@ -15,40 +15,52 @@ function parseToDo(array) {
     let result = []
     for (let toDo of array) {
         let curr = [toDo];
-        let toDoTable = "";
-        if(toDo.indexOf(';') !== -1)
-        {
+        if (toDo.indexOf(';') !== -1) {
             curr.push(toDo.split(';')[0].trim().split(' ').at(-1).toLowerCase());
             curr.push(new Date(toDo.split(';')[1].trim()));
-        }
-        else
-        {
+        } else {
             curr.push(null);
             curr.push(null);
         }
-        if(toDo.indexOf('!')!==-1)
-        {
+        if (toDo.indexOf('!') !== -1) {
             curr.push(true);
-            toDoTable += '!  ';
-        }
-        else
-        {
+        } else {
             curr.push(false);
-            toDoTable += '   ';
         }
         result.push(curr);
     }
     return result;
 }
 
-function getToDo(){
+function getTableView(array) {
+    let newStr = "";
+    if (array.at(-1)) {
+        newStr += ' !  |';
+    } else {
+        newStr += '    |';
+    }
+    newStr += array[0].split(';').at(-1) + "  |";
+    if (array.at(1)!==null) {
+        newStr += array.at(1) + '  |';
+    } else {
+        newStr += "    |";
+    }
+    if (array.at(2)!==null) {
+        newStr += array.at(2) + '  |';
+    } else {
+        newStr += "  |";
+    }
+    return newStr;
+}
+
+function getToDo() {
     const pattern = new RegExp(/\/\/ TODO .*/)
     let files = getFiles();
     let result = [];
-    for(let file of files){
-        for(let line of file.split("\n")){
+    for (let file of files) {
+        for (let line of file.split("\n")) {
             let matching = line.match(pattern);
-            if(matching !== null)
+            if (matching !== null)
                 result.push(matching[0]);
         }
     }
@@ -61,16 +73,16 @@ function processCommand(command) {
             process.exit(0);
             break;
         case 'show':
-            console.log(getToDo());
+            console.log(parseToDo(getToDo()).map(x=>getTableView(x)));
             break;
         case 'important':
-            console.log(parseToDo(getToDo()).filter(x=>x.at(-1)).map(x=>x.at(0)));
+            console.log(parseToDo(getToDo()).filter(x => x.at(-1)).map(x => x.at(0)));
             break;
         case 'user':
-            console.log(parseToDo(getToDo()).filter(x=>x.at(1) === command.split(' ').at(1).toLowerCase()).map(x=>x.at(0)));
+            console.log(parseToDo(getToDo()).filter(x => x.at(1) === command.split(' ').at(1).toLowerCase()).map(x => x.at(0)));
             break;
         case 'date':
-            console.log(parseToDo(getToDo()).filter(x=>x.at(2) != null && x.at(2) < new Date(command.split(' ').at(1))).map(x=>x.at(0)));
+            console.log(parseToDo(getToDo()).filter(x => x.at(2) != null && x.at(2) < new Date(command.split(' ').at(1))).map(x => x.at(0)));
             break;
         default:
             console.log('wrong command');
