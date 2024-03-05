219c993c70361df2ed7b4a8c417b96e4f33998d6
ricovaniy <115656372+ricovaniy@users.noreply.github.com>
2024-03-05 18:58:40 +0500

    task3

--- fileSystem.js
+++ fileSystem.js
@@ -26,3 +26,4 @@ module.exports = {
     getAllFilePathsWithExtension,
     readFile,
 };
+// TODO ТИПО ТЕСТ ИМПОРТАНТА!!
--- index.js
+++ index.js
@@ -12,9 +12,37 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    const parts = command.split(' ');
+    const comments = getAllComments();
+    switch (parts[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(comments.join(''))
+            break;
+        case 'important':
+            let important = comments.filter(x => x.includes('!'));
+            console.log(important.join(''));
+            break;
+        case 'user':
+            if (parts.length === 1)
+                console.error('wrong command');
+            const name = parts.slice(1).join(' ')
+            const d = getUserMessage(comments, 1);
+            console.log(d.get(name).join(''))
+            break;
+        case 'sort':
+            sortCom(parts[1], comments);
+            break;
+        case 'date':
+            if (parts.length === 1)
+                console.error('wrong command');
+            const dict = getUserMessage(comments, 2);
+            const sortedKeys = getSortedDateKeys(dict).filter(x => x >= parts[1]);
+            for (const key of sortedKeys){
+                console.log(`${key}: ${dict.get(key).join('')}`)
+            }
             break;
         default:
             console.log('wrong command');
@@ -22,4 +50,65 @@ function processCommand(command) {
     }
 }
 
-// TODO you can do it!
+function getAllComments() {
+    let arr = [];
+    const regexp = /(\/\/ TODO .+)(\r\n)*/g;
+    for (const prog of files) {
+        const comments = prog.match(regexp);
+        arr = comments ? arr.concat(comments) : arr;
+    }
+    return arr;
+}
+
+function sortCom(param, comments){
+    switch (param){
+        case 'importance':
+            console.log(comments.filter(x => x.includes('!')).join(''))
+            console.log(comments.filter(x => !x.includes('!')).join(''))
+            break;
+        case 'user':
+            const d = getUserMessage(comments, 1);
+            for (const entry of d.entries()){
+                if (entry[0] !== 'unknown') {
+                    console.log(`${entry[0]}: \n${entry[1].join('')}`);
+                }
+            }
+            console.log(`unknown: \n${d.get('unknown').join('')}`);
+            break;
+        case 'date':
+            const dateDict = getUserMessage(comments, 2);
+            let sortedKeys = getSortedDateKeys(dateDict);
+            for (const key of sortedKeys){
+                console.log(`${key}: ${dateDict.get(key).join('')}`);
+            }
+    }
+}
+
+function dataSorter(a, b){
+    const dateA = new Date(a);
+    const dateB = new Date(b);
+    return dateB.getTime() - dateA.getTime();
+}
+function getSortedDateKeys(dict){
+    return Array.from(dict.keys()).sort(dataSorter);
+}
+function getUserMessage(comments, argNum){
+    const reg = /^\s*\/\/ TODO (.+); (\d{4}-\d{2}-\d{2}); (.+)$/;
+    const dict = new Map();
+    for (const str of comments) {
+        const matches = str.trimEnd().match(reg);
+        if (matches) {
+            const name = matches[argNum].toLowerCase();
+            if(!dict.has(name)){
+                dict.set(name, [])
+            }
+            dict.get(name).push(str)
+        } else {
+            if (!dict.has('unknown')){
+                dict.set('unknown', [])
+            }
+            dict.get('unknown').push(str);
+        }
+    }
+    return dict;
+}
