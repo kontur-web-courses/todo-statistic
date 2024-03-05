ed5ea2f776f43dca559bfd863a153fa163b072c8
Pavel Vasiliev <vasilievpavel4@gmail.com>
2024-03-05 18:12:35 +0500

    getTodos

--- index.js
+++ index.js
@@ -11,10 +11,36 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTodos(includedText=null){
+    let paths = getAllFilePathsWithExtension(process.cwd(), 'js');
+    let todos = [];
+    for (let path of paths){
+        let file = readFile(path);
+        for (let line of file.split('\n')){
+            if (line.includes('// TODO') && (includedText == null || line.includes(includedText))){
+                let splittedLine = line.split('//');
+                // console.log(splittedLine);
+                let todo = '//' + splittedLine[splittedLine.length - 1];
+                // console.log(todo);
+                todos.push(todo)
+            }
+        }
+    }
+    return todos;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            let todos = getTodos();
+            console.log(todos);
+            break;
+        case "important":
+            let importantTodos = getTodos('!');
+            console.log(importantTodos);
             break;
         default:
             console.log('wrong command');
