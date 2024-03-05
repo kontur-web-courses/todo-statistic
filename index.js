eabca7c601ed16c05324e277dd9a0fe46024c3ea
Alexander Sapegin <sapegin04@mail.ru>
2024-03-05 18:02:54 +0500

    Add getTodos function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const TODO_REGEXP = /\/\/ TODO.*/gm;
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -9,6 +10,10 @@ readLine(processCommand);
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
+}
+
+function getTodos(file) {
+    return file.match(TODO_REGEXP);
 }
 
 function processCommand(command) {
