4fb2beb5aecea2f3e72adeaf09ec8341eb38b47e
ISich <arseny.voroni@yandex.ru>
2024-03-05 17:58:30 +0500

    add show

--- index.js
+++ index.js
@@ -11,10 +11,31 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getToDO(){
+    let res = []
+    let lines = getFiles();
+    
+    for (let q = 0; q < lines.length; q++){
+        let txt = lines[q].split('\r\n');
+        //console.log(txt);
+        for (let i = 0; i < txt.length; i++) {
+            let ind = txt[i].indexOf("// TODO"); 
+            if (ind != -1) {
+                //console.log(txt[i]);
+                res.push(txt[i].slice(ind+8));
+            }
+        }
+    }
+    return res
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(getToDO());
             break;
         default:
             console.log('wrong command');
