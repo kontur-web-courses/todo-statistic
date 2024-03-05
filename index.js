4bcf7adb0ada2a55b8eb08903a89375e2e853c0f
vVladEr <vlader.2004.ekb@gmail.com>
2024-03-05 19:18:16 +0500

    task 7, 8 complete

--- index.js
+++ index.js
@@ -11,19 +11,25 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command.split(' ')[0]) {
+    splittedCommand = command.split(' ')
+    switch (splittedCommand[0]) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            printArray(getTODOLines());
+            printArray(getFormatLines(getTODOLines()));
             break;
         case 'important' :
-            printArray(getImportantLines(getTODOLines()));
+            printArray(getImportantLines(getFormatLines(getTODOLines())));
             break;
         case 'user':
-            let user = command.split(' ')[1];
+            let user = splittedCommand[1];
             printArray(getUserLines(getFormatLines(getTODOLines()), user));
+            break;
+        case 'sort':
+            let key = splittedCommand[1];
+            const arr = sortByKey(getFormatLines(getTODOLines()), key);
+            printArray(arr.reverse());
             break;
         default:
             console.log('wrong command');
@@ -50,7 +56,7 @@ function getTODOLines()
 function getImportantLines(lines){
     const res = new Array();
     for(const line of lines){
-        if(line.indexOf('!') !== -1){
+        if(line.importance){
             res.push(line);
         }
     }
@@ -58,8 +64,16 @@ function getImportantLines(lines){
 }
 
 function printArray(arr){
+    let maxUserLength = 0;
     for(const item of arr){
-        console.log(item);
+        maxUserLength = Math.max(item.user.length, maxUserLength);
+    }
+    for(const item of arr){
+        let importanceLine = item.importance ? '! ' : '  ';
+        let userLine = ` ${item.user}`;
+        userLine  = userLine.padEnd(maxUserLength + 1);
+        userLine += ' ';
+        console.log(`${importanceLine}|${userLine}| ${item.date} | ${item.text}`);
     }
 }
 
@@ -71,7 +85,7 @@ function getFormatLines(lines) {
             user : '',
             date : '',
             text : '',
-            important : false
+            importance : false
         };
         let a = line.split(";");
         if (a.length - 1 === 2) {
@@ -80,7 +94,7 @@ function getFormatLines(lines) {
             data.text = a[2];
         }
         if(line.indexOf('!') !== -1){
-            data.important = true;
+            data.importance = true;
         }
         formatLines.push(data);
     }
@@ -95,4 +109,12 @@ function getUserLines(formatLines, user) {
         }
     }
     return result;
+}
+
+function sortByKey(arr, key){
+    return arr.sort(function(a, b){
+        const x = a[key];
+        const y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
 }
