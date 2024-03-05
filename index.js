0ec4fc0f6e53d309d9f6af11b0db1b8ce9baaf91
Klimov_Dmitriy <dmitriyklimov98@yandex.ru>
2024-03-05 18:25:50 +0500

    show

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const KEYWORD = '// TODO '; // скипни ту хуйню
+
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
@@ -16,10 +18,31 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            getAllComments();
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getAllComments() {
+    const comments = []
+    for (const file of files) {
+        const lines = file.split('\n');
+        for (const line of lines) {
+            const i = line.indexOf(KEYWORD);
+            if (i != -1) {
+                let sub = line.slice(i + KEYWORD.length, -1);
+                if (!sub.includes('// скипни ту хуйню')) {
+                    comments.push(line.slice(i + KEYWORD.length, -1));
+                }
+            }
+        }
+    }
+    console.log(comments);
+    return comments;
 }
 
 // TODO you can do it!
