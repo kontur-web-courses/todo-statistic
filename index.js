96f7a733a8c036b787b131560e5a9bcc19428e21
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2024-03-05 17:58:44 +0500

    Add show command

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const comments = [];
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -11,13 +12,29 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getComments() {
+    for (let file of files) {
+        const regex = /\/\/ TODO (.+?)(?=\r\n|\n|$)/g;
+        const matches = [...file.matchAll(regex)];
+        for (const match of matches) {
+             comments.push(match.toString());
+        }
+    }
+}
+
 function processCommand(command) {
+    getComments();
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            for (let comment of comments)
+                console.log(comment);
+            break;
+
         default:
-            console.log('wrong command');
+            console.log('wrong command123');
             break;
     }
 }
