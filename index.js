13ffa3ed0ac01cdbd047dd98cb768f30568234db
VladimirLezhnin <115662980+VladimirLezhnin@users.noreply.github.com>
2024-03-05 19:54:40 +0500

    123131

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function getFiles() {
 
 function getComments() {
     for (let file of files) {
-        const regex = /\/\/ TODO (.+?)(?=\r\n|\n|$)/g;
+        const regex = /\/\/\s*[tT][oO][dD][oO]\s*:?([^\n]*)/g;
         const matches = [...file.matchAll(regex)];
         for (const match of matches) {
              comments.push(match[0].toString());
@@ -35,11 +35,62 @@ function findUserIndex(str, user) {
 function processCommand(command) {
     getComments();
     let commandParams = command.split(' ');
-    command = commandParams[0];
-    switch (command) {
+    let command1 = commandParams[0];
+    switch (command1) {
         case 'exit':
             process.exit(0);
             break;
+        case 'date':
+            const regex = /(\d{4}(?:-\d{2})?(?:-\d{2})?)/;
+            const match = command.match(regex);
+            if (match) {
+                const date = match[1];
+                console.log(date);
+                let commandDate = new Date(date);
+
+                for (let comment of comments) {
+                    const regex2 = /\b(\d{4})-(\d{2})-(\d{2})\b/;
+                    let commentDataStr = comment.match(regex2);
+                    if (commentDataStr !== null) {
+                        let commentDate = new Date(commentDataStr[1]);
+                        if (commentDate > commandDate)
+                            console.log(comment)
+                    }
+
+                }
+
+            }
+            break;
+
+        case 'sort':
+            if (commandParams[1] === 'date') {
+
+
+                let arr = comments.sort((a, b) => {
+                    if (a.match(/\b(\d{4})-(\d{2})-(\d{2})\b/) === null)
+                        return 10000;
+                    if (b.match(/\b(\d{4})-(\d{2})-(\d{2})\b/) === null)
+                        return 10000;
+
+                    let dateA = new Date(a.match(/\b(\d{4})-(\d{2})-(\d{2})\b/)[0]).getTime();
+                    let dateB = new Date(b.match(/\b(\d{4})-(\d{2})-(\d{2})\b/)[0]).getTime();
+
+                    if (dateA > dateB) {
+                        return 1;
+                    }
+                    if (dateA < dateB) {
+                        return -1;
+                    }
+                    if (dateA === dateB) {
+                        return 0;
+                    }
+                } )
+
+                for (let comment of arr)
+                    console.log(comment)
+
+                break;
+            }
         case 'show':
             for (let comment of comments)
                 console.log(comment);
