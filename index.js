d9b114c6d7488333e381625d2b1cb6a7e730a0fa
dashakonovalova <115693972+dashakonovalova@users.noreply.github.com>
2024-03-05 18:15:57 +0500

    2
    2

--- index.js
+++ index.js
@@ -16,6 +16,18 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            const comments = [];
+            for (const file of files) {
+                const lines = file.split('\n');
+                for (const line of lines) {
+                    if (line.trim().startsWith('// TODO')) {
+                        comments.push(line.replace('// TODO', '').trim());
+                    }
+                }
+            }
+            console.log(comments);
+            break;
         default:
             console.log('wrong command');
             break;
