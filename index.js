16a214d162fbd2ae868c48b83792e702c2276f91
ollxko <i.ollxko.ru>
2024-03-05 18:32:51 +0500

    '4'

--- index.js
+++ index.js
@@ -12,19 +12,38 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    switch (command.split(' ')[0]) {
         case 'show':
             showTodoComments();
             break;
         case 'important':
             showTodoComments(true)
-       case 'exit':
+        case `user`:
+            showUserComments(username);
+        case 'exit':
             process.exit(0);
             break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function showUserComments(username) {
+    const todoComments = [];
+
+    files.forEach(fileContent => {
+        const lines = fileContent.split('\n');
+        lines.forEach(line => {
+            if (line.includes('// TODO')) {
+                const splittedLine = line.split(' ');
+                if (splittedLine[1][-1] === ';' && splittedLine[2][-1] === ';' && splittedLine[1].includes(username)) {
+                    todoComments.push(line);
+                }
+            }
+        });
+    });
+    todoComments.forEach(comment => console.log(comment));
 }
 
 function showTodoComments(isImportant) {
@@ -36,8 +55,7 @@ function showTodoComments(isImportant) {
             if (line.includes('// TODO')) {
                 if (isImportant && line.includes('!')) {
                     todoComments.push(line.slice(line.indexOf('// TODO')));
-                }
-                else if (!isImportant){
+                } else if (!isImportant) {
                     todoComments.push(line.slice(line.indexOf('// TODO')));
                 }
             }
@@ -45,4 +63,5 @@ function showTodoComments(isImportant) {
     });
     todoComments.forEach(comment => console.log(comment));
 }
+
 // TODO you can do it!
