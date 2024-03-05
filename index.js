64816243308ec10789aef1c97b1be3a02e224665
Insomniaccc <92261519+Insomniacccc@users.noreply.github.com>
2024-03-05 19:07:09 +0500

    task done

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    let args = command.split(' ').slice(1);
+    let arg = args[0]
+    let commands = command.split(' ')[0];
+    switch (commands) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            show(); 
+            break;
+        case 'user':
+            user(arg);
+            break;
+        case "important":
+            important();
+            break;
+        case 'sort':
+            sort(arg);
+            break
+        case 'date':
+            date(arg);
             break;
         default:
             console.log('wrong command');
@@ -22,4 +40,61 @@ function processCommand(command) {
     }
 }
 
+
+
+
+
+
+function show() {
+    console.log(getToDos());
+}
+
+function getToDos() {
+    const list = getFiles();
+    return list.flatMap(x => x.match(/\/\/ TODO.*/g) || []);
+}
+
+
+function user(username) {
+    files.map(x => x.match(new RegExp(`\/\/ TODO ${username};.*;.*`, 'gi')))
+        .reduce((a, b) => b === null ? a : a.concat(b), [])
+        .forEach(x => console.log(x));
+}
+
+function important() {
+    const list = getFiles();
+    list.flatMap(x => x.match(/\/\/ TODO.*!.*/g) || [])
+        .forEach(x => console.log(x));
+}
+
+function sort(arg) { 
+    let todos = getToDos()
+    switch (arg) {
+        case "important":
+            {
+                let list = getFiles();
+                important();
+                list.map(x => x.match(/\/\/ TODO[^!\r\n]+\r/g)).reduce(function (a, b) {
+                    return b === null ? a : a.concat(b);
+                }, []).sort((a, b) => a.split('!').length < b.split('!').length)
+                    .forEach(x => console.log(x));
+                break;
+            }
+        
+        case 'user':
+            {
+                return todos.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+                    .sort((a, b) => b.split(";").length - a.split(";").length).forEach(x => console.log(x));
+                break;
+            }
+
+        case 'date':
+            {
+                break
+            }
+
+    }
+}
 // TODO you can do it!
+
+
