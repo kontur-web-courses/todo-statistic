6bb568e0d67570d27c55071a314e2c0c919c1e9d
vVladEr <vlader.2004.ekb@gmail.com>
2024-03-05 18:05:59 +0500

    tasks 1 and 2 completed

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
-
 console.log('Please, write your command!');
 readLine(processCommand);
 
@@ -16,10 +15,25 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            getTODOLines();
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
 
+function getTODOLines()
+{
+    for(let file of files){
+        let lines = file.split('\n');
+        for(let line of lines){
+            const index = line.indexOf("// TODO")
+            if(index !== -1){
+                console.log(line.slice(index));
+            }
+        }
+    }
+}
 // TODO you can do it!
