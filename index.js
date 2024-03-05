fee53b5648b90534648b026cdef4450c5dface39
Amyton0 <khlopina.sd@gmail.com>
2024-03-05 18:38:59 +0500

    .

--- index.js
+++ index.js
@@ -18,16 +18,44 @@ function processCommand(command) {
             process.exit(0);
             break;
         case 'show':
-            for (const fileText of files) {
-                for (const todo of extract.todos(fileText)) {
-                    console.log(`TODO: ${todo.text}`)
-                }
-            }
-            break
+            let toDoAll = getToDo();
+            printToDo(toDoAll)
+            break;
+        case 'important':
+            let toDoImportant = getToDo();
+            printToDo(toDoImportant.filter(function (s) {
+                return s.includes("!")
+              }))
+            break;
+        case /user ./:
+            let toDoWithAuthors = getToDo();
+            let user = command.split(' ')[1];
+            printToDo(toDoImportant.filter(function (s) {
+                return s.split(';')[0] === user && s.split(';').length == 3
+              }))
+            break;
         default:
             console.log('wrong command');
             break;
     }
+}
+
+function getToDo() {
+    let strings = [];
+    
+    for (const fileText of files) {
+        for (const todo of extract.todos(fileText)) {
+            strings.push(todo.text);
+        }
+    }
+    return strings;
+}
+
+function printToDo(toDo)
+{
+    for (let i = 0; i < toDo.length; i++) {
+        console.log(toDo[i])
+      }
 }
 
 // TODO you can do it!
