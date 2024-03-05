8e72254cbe28a9233289df8c54094b7322b9d9e2
antoshkaxxr <63500718+antoshkaxxr@users.noreply.github.com>
2024-03-05 17:26:03 +0500

    base functionality

--- index.js
+++ index.js
@@ -16,10 +16,51 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
-        default:
-            console.log('wrong command');
+        case 'show todo':
+            const todos = getTodos();
+            todos.forEach(todo => {
+                console.log(`${todo}\n`);
+            });
+            break;
+        case 'important todo':
+            const allTodos = getTodos();
+            allTodos.forEach(todo => {
+                if (todo.includes("!")) {
+                    console.log(`${todo}\n`);
+                }
+            });
             break;
     }
+
+    if (command.includes("user"))
+    {
+        const allUserTodos = getTodos();
+        const userName = command.split(" ")[1];
+
+        allUserTodos.forEach(todo => {
+            const curUserName = todo.substring(8, todo.indexOf(";"));
+            if (userName.toLowerCase() === curUserName.toLowerCase()) {
+                console.log(todo);
+            }
+        });
+    }
+}
+
+function getTodos() {
+    const todos = [];
+    files.forEach(file => {
+        const lines = file.split('\n');
+        lines.forEach(line => {
+            if (line.includes('// TODO')) {
+                const comment = line.slice(line.indexOf('// TODO'));
+                if (comment[7] === " ") {
+                    todos.push(comment);
+                }
+            }
+        });
+    });
+
+    return todos;
 }
 
 // TODO you can do it!
