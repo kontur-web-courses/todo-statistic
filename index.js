875fb268189ce8a41f8a5f2d95d20a29b40ecfb0
Danil <ferrum268@mail.ru>
2024-03-05 18:15:07 +0500

    1, 2 tasks

--- index.js
+++ index.js
@@ -11,10 +11,34 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function searchAllFiles(files){
+    let result = [];
+    for (let file of files){
+        result.push(...findAllTodos(file));
+    }
+    return result;
+}
+
+function findAllTodos(file){
+    let result = [];
+    let lines = file.split('\n');
+    for(let line of lines){
+        let index = line.indexOf('// TODO ');
+        if (index !== -1){
+            result.push(line.slice(index));
+            console.log(line.slice(index));
+        }
+    }
+    return result;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            console.log(searchAllFiles(getFiles()));
             break;
         default:
             console.log('wrong command');
