d4ef6f3a43c2993a3cf16f36ea1b4bb7614c36bb
Kol1n13 <60143734+Kol1n13@users.noreply.github.com>
2024-03-05 18:50:38 +0500

    my

--- index.js
+++ index.js
@@ -12,14 +12,95 @@ function getFiles() {
 }
 
 function processCommand(command) {
-    switch (command) {
+    command = command.split(' ');
+    switch (command[0]) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (let comment of getTODOcomments(getFiles()).map(TODO => TODO.comment)){
+                console.log('');
+                console.log(comment);
+                
+            }
+            break;
+        case 'important':
+            for (let comment of getTODOcomments(getFiles()).filter(TODO => TODO.importance > 0).map(TODO => TODO.comment)){
+                console.log('');
+                console.log(comment);
+            }
+            break;
+        case 'user':
+            for (let comment of getTODOcomments(getFiles())
+            .filter(TODO => TODO.user !== undefined && TODO.user.toLowerCase() === command[1].toLowerCase())
+            .map(TODO => TODO.comment)){
+                console.log('');
+                console.log(comment);
+            }
+            break;
+        case 'sort':
+            let array = getTODOcomments(getFiles());
+            if (command[1] === 'importance'){
+                array.sort((a, b) => b.importance - a.importance);
+            }
+            for (let elem of array){
+                console.log(elem.comment);
+            }
             break;
         default:
             console.log('wrong command');
             break;
     }
 }
+
+function getTODOcomments(files){
+    let TODO = [];
+    for (let file of files){
+        let tempFile = file;
+        while (true){
+            
+            let index = tempFile.indexOf('// TODO ');
+            if (index === -1){
+                break;
+            }
+            tempFile = tempFile.slice(index);
+            let nIndex = tempFile.indexOf('\n');
+            if (nIndex === -1){
+                TODO.push(createTODOObject(tempFile.trim()))
+                break;
+            }
+            else{
+                TODO.push(createTODOObject(tempFile.slice(8, nIndex).trim()))
+                tempFile = tempFile.slice(nIndex);
+            }
+        }
+    }
+    return TODO;
+}
+
+function createTODOObject(TODOComment) {
+    let TODOobject = { user: " ",
+                        date: " ",
+                        importance: 0,
+                        comment: ' '
+    };
+    let ops =  TODOComment.split(';').map(
+        function (tag) {
+         return tag.trim()});
+    TODOobject.comment = TODOComment;
+    for (let e of ops.slice(0, -1)){
+        let probableDate = Date.parse(e)
+        if (Number.isNaN(probableDate)){
+            TODOobject.user = e;
+        }
+        else{
+            TODOobject.date = probableDate;
+        }
+    }
+    let impTemp = TODOobject.comment.match(new RegExp('!', 'g'));
+    TODOobject.importance = impTemp === null ? 0 : impTemp.length;
+    return TODOobject;
+}
+
 
 // TODO you can do it!
