c82772dc5ea182d1c2ca91aee87314a18451e80f
DuninD <dunin-04@mail.ru>
2024-03-05 20:58:47 +0500

    add 2 sort command

--- index.js
+++ index.js
@@ -31,15 +31,25 @@ function processCommand(command) {
             })
             break;
         case 'user':
-            if (command.length > 0){
-                getComments().forEach(comment => {
-                    if ((match = nameDataRegex.exec(comment)) !== null){
-                        if (match[1].toLowerCase() === cmd[1].toLowerCase()){
-                            console.log(comment);
-                        }
-                    }
-                })
+            if (cmd.length > 1){
+                getUsers(cmd[1]);
             }
+            break;
+        case 'sort':
+                switch (cmd[1]){
+                    case 'importance':
+                        sortByImportance();
+                        break;
+                    case 'user':
+                        sortByUsers();
+                        break;
+                    case 'date':
+                        sortByDate();
+                        break;
+                    default:
+                        console.log('wrong sort option');
+                        break;
+                }
             break;
         default:
             console.log('wrong command');
@@ -56,5 +66,82 @@ function getComments(){
         }
     });
     return comments;
+}
+
+function sortByCharCount(arr, char) {
+    let charCountMap = {};
+    arr.forEach(str => {
+        charCountMap[str] = str.split(char).length - 1;
+    });
+    arr.sort((a, b) => charCountMap[b] - charCountMap[a]);
+    return arr;
+}
+
+function dateSorting(arr) {
+    let dateMap = {};
+    for (const comment of arr) {
+        let match;
+        if ((match = nameDataRegex.exec(comment)) !== null){
+            dateMap[comment] = match[2];
+        }
+    }
+    console.log(dateMap);
+    arr.sort((a, b) => +(new Date(dateMap[b]) - new Date(dateMap[a])));
+    return arr;
+}
+
+function getUsers(user){
+    getComments().forEach(comment => {
+        if ((match = nameDataRegex.exec(comment)) !== null){
+            if (match[1].toLowerCase() === user.toLowerCase()){
+                console.log(comment);
+            }
+        }
+    })
+}
+
+function sortByImportance() {
+    let important = [];
+    let notImportant = [];
+    getComments().forEach(comment => {
+        if (comment.indexOf('!') !== -1){
+            important.push(comment);
+        } else {
+            notImportant.push(comment);
+        }
+    })
+    important = sortByCharCount(important, '!');
+    important.forEach(comment => console.log(comment));
+    notImportant.forEach(comment => console.log(comment));
+}
+
+function sortByUsers(){
+    let withUser = [];
+    let withoutUser = [];
+    getComments().forEach(comment => {
+        if ((match = nameDataRegex.exec(comment)) !== null){
+            withUser.push(comment);
+        } else {
+            withoutUser.push(comment);
+        }
+    })
+    withUser.forEach(comment => console.log(comment));
+    withoutUser.forEach(comment => console.log(comment));
+}
+
+function sortByDate() {
+    let withDate = [];
+    let withoutDate = [];
+        for (const file of getComments()) {
+            let res = file.match(nameDataRegex)
+            if (res !== null){
+                withDate.push(file);
+            } else {
+                withoutDate.push(file);
+            }
+        }
+    withDate = dateSorting(withDate);
+    withDate.forEach(comment => console.log(comment));
+    withoutDate.forEach(comment => console.log(comment));
 }
 // TODO you can do it!
