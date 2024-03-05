3fa45d10855245a3d74368f79a1b6161151f40f8
Danil <ferrum268@mail.ru>
2024-03-05 18:31:56 +0500

    3 task

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function searchAllFiles(files){
 
 function findAllTodos(file){
     let result = [];
-    let lines = file.split('\n');
+    let lines = file.split('\r\n');
     for(let line of lines){
         let index = line.indexOf('// TODO ');
         if (index !== -1){
@@ -33,13 +33,38 @@ function findAllTodos(file){
     return result;
 }
 
+function markTodosImportance(todos){
+    for (let todo of todos){
+        todo.important = todo.text.includes('!');
+    }
+}
+
+function findImportantTodos(todos){
+    result = [];
+    for (let todo of todos){
+        if (todo.important){
+            result.push(todo);
+        }
+    }
+    return result;
+}
+
+function printText(todos){
+    console.log(todos.map(obj => obj.text));
+}
+
 function processCommand(command) {
+    const todos = searchAllFiles(getFiles());
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            console.log(searchAllFiles(getFiles()).map(obj => obj.text));
+            printText(todos);
+            break;
+        case 'important':
+            markTodosImportance(todos);
+            printText(findImportantTodos(todos));
             break;
         default:
             console.log('wrong command');
