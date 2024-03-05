7bbbd2840f70e692b9db5bf3590585358ba68e83
bober19 <115493537+Manykeys@users.noreply.github.com>
2024-03-05 17:46:25 +0500

    2 step

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
+const toDo = [];
 
 console.log('Please, write your command!');
 readLine(processCommand);
@@ -16,10 +17,29 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show : показать все todo':
+            func();
+            for (let res of toDo)
+                console.log(res);
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
+
+function func() {
+    for (let file of files) {
+        let lines = file.split('\n');
+
+        for (let line of lines) {
+            if (line.trim().startsWith('// TODO')) {
+                let todoSubstring = line.substring(line.indexOf('// TODO') + 8);
+                toDo.push('// TODO ' + todoSubstring.trim())
+            }
+        }
+    }
+}
+
 
 // TODO you can do it!
