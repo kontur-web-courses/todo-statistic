02568cbab8c7b7fabb20c20b86cd36cdd1c75696
AIM1rage <michael050100@gmail.com>
2024-03-05 18:33:50 +0500

    task 1 done

--- index.js
+++ index.js
@@ -11,10 +11,30 @@ function getFiles() {
     return filePaths.map(path => readFile(path));
 }
 
+function getTodoComment(line) {
+    let splittedLine = line.split('//', 2);
+    if (splittedLine.length > 1 && splittedLine[1].trim().startsWith('TODO')) {
+        return '//' + splittedLine[1].trimEnd();
+    }
+    return null;
+}
+
 function processCommand(command) {
     switch (command) {
         case 'exit':
             process.exit(0);
+            break;
+        case 'show':
+            for (file of files) {
+                for (line of file.split('\n')) {
+                    let comment = getTodoComment(line);
+                    if (comment !== null)
+                        console.log(comment);
+                }
+            }
+            break;
+        case 'important':
+            // TODO
             break;
         default:
             console.log('wrong command');
