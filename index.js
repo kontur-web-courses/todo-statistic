01dd180debf450baa3b631029c7a28c51f824f6f
yellooot <njkzrhjkz@gmail.com>
2024-03-05 19:06:33 +0500

    Add 6

--- index.js
+++ index.js
@@ -23,9 +23,21 @@ function processCommand(command) {
                 const userTodos = getTodosByUser(commandParts[1]);
                 userTodos.forEach(todo => console.log(todo));
             }
+            else  {
+                console.log('Need username');
+            }
             break;
         case 'show':
             console.log(getTODOComments());
+            break;
+        case 'date':
+            if (commandParts.length === 2) {
+                const dateComments = getCommentsAfterDate(String(commandParts[1]))
+                dateComments.forEach(comment => console.log(comment));
+            }
+            else  {
+                console.log('Need date');
+            }
             break;
         case 'exit':
             process.exit(0);
@@ -36,12 +48,13 @@ function processCommand(command) {
     }
 }
 
-const regex = /\/\/\s*TODO\s.*/g;
+const TODORegex = /\/\/\s*TODO\s.*/g;
+const dateRegex = /(\d{4}(-\d{2}(-\d{2})?)?)/
 function getTODOComments() {
     let result = [];
     let matches;
     for (let file of files) {
-        matches = file.match(regex);
+        matches = file.match(TODORegex);
         if (matches)
             result = result.concat(matches.map(line => line.slice(8)));
     }
@@ -59,4 +72,25 @@ function getTodosByUser(username) {
         const todoParts = todo.split(';').map(part => part.trim());
         return todoParts[0].toLowerCase() === username.toLowerCase();
     });
+}
+
+function extractDateFromComment(comment) {
+    const match = comment.match(dateRegex);
+    return match ? match[1] : null;
+}
+
+function getCommentsAfterDate(date) {
+    const targetDate = new Date(date);
+    let result = [];
+    let comments = getTODOComments();
+    for (let comment of comments) {
+        const commentDateString = extractDateFromComment(comment);
+        if (commentDateString) {
+            const commentDate = new Date(commentDateString);
+            if (commentDate > targetDate) {
+                result.push(comment);
+            }
+        }
+    }
+    return result;
 }
